0454100c9e8a6bbf575dea7f87814d98247900db
Carlos Alonso Torres <calonso@Linuxmintconabio>
2013-07-22 17:53:27 -0500

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Buscador::Application.config.secret_key_base = '82e72ea5d2b63c180cf0ec900e82e9855fd8a1ebbc89649c40486f1d65552f21b83a60af126b1b9e7ac84149c5e28aaee9e07dbdef020ae1bd2bedaf6ab27e02'
