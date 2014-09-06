db8309578c5f2487a04847ca463f6cc3cbd3313d
Carlos Alonso Torres <calonso@calonso-debian>
2014-09-05 20:03:47 -0500

    cambios en los webservices

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,4 +25,3 @@
 //= require general
 //= require autocomplete-rails
 
-
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,14 +4,14 @@ Buscador::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = false            #esta linea es para que cargue el cache
 
   # Do not eager load code on boot.
   config.eager_load = false
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
