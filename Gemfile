6048d13afce44bbd9d6ac332afa010431e000bfa
Carlos Alonso Torres <calonso@calonso-debian>
2014-11-07 11:41:50 -0600

    scripts del proveedor

--- Gemfile
+++ Gemfile
@@ -31,13 +31,11 @@ gem 'pg'
 gem 'rack-contrib'
 gem 'rails', github: 'rails/rails', branch: '4-0-stable'
 gem 'rails3-jquery-autocomplete', github: 'calonso-conabio/rails3-jquery-autocomplete', branch: 'master'   #autocompleta las busquedas
-#gem 'rcore-ext'
 gem 'rest-client', :require => 'rest_client'
 gem 'rinku', :require => 'rails_rinku'    #extiende el metodo auto_link
 gem 'sass-rails', '~> 4.0.0'
 gem 'savon', '2.4.0'       #para consumir webservices con SOAP y WSDL
 gem 'seed_dump'   #extrae las tuplas de un modelo o de toda la base
-#gem 'simple_datatables'   #simple datatables para hacer el buscador
 gem 'soulmate', :require => 'soulmate/server'
 gem 'therubyracer', platforms: :ruby
 gem 'thinking-sphinx'     #busca parlabras completas en milisegundos, solo para mysql y postgres
@@ -48,7 +46,6 @@ gem 'ts-delayed-delta', '~> 2.0.0'    #funciona con thinking-sphinx
 gem 'uglifier', '>= 1.3.0'
 gem 'wash_out'
 gem 'will_paginate'    #hace la paginacion de una rango determinado
-#gem 'ya2yaml'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- app/models/proveedor.rb
+++ app/models/proveedor.rb
@@ -28,4 +28,66 @@ class Proveedor < ActiveRecord::Base
 
     nombres_comunes_faltan
   end
+
+  def fotos(usuario)
+    datos = eval(naturalista_info)
+    datos = datos.first if datos.is_a?(Array)
+    return [] unless datos['taxon_photos'].present?
+    return [] if usuario.blank?
+
+    fotos_naturalista= taxon_photos(datos, usuario)
+    return [] if fotos_naturalista.length == 0
+    taxon = especie
+    fotos_buscador = taxon.photos
+
+    if fotos_buscador
+      # Para no borrar las anteriores fotos
+      fotos_naturalista.each do |photo|
+        photo.save
+        taxon_photo = TaxonPhoto.new(:especie_id => taxon.id, :photo_id => photo.id)
+        taxon_photo.save
+      end
+    else
+      fotos_buscador = fotos_naturalista
+      taxon.save
+    end
+  end
+
+  private
+
+  def photo_type(url)
+    return 'FlickrPhoto' if url.include?("staticflickr\.com") || url.include?("static\.flickr\.com")
+    return 'EolPhoto' if url.include? "media\.eol\.org"
+    return 'NaturalistaPhoto' if url.include? "static\.inaturalist\.org"
+    return 'WikimediaCommonsPhoto' if url.include? "upload\.wikimedia\.org"
+  end
+
+  def taxon_photos(datos, usuario)
+    photos = []
+    datos['taxon_photos'].each do |pho| #Guarda todas las fotos asociadas del taxon
+      next unless pho['photo']['native_photo_id'].present?
+      next unless pho['photo']['thumb_url'].present?
+
+      local_photo = Photo.where(:native_photo_id => pho['photo']['native_photo_id'], :type => photo_type(pho['photo']['thumb_url']))
+      photo = local_photo.count == 1 ? local_photo.first : Photo.new #Crea o actualiza la foto
+
+      photo.usuario_id = usuario
+      photo.native_photo_id = pho['photo']['native_photo_id']
+      photo.square_url = pho['photo']['square_url']
+      photo.thumb_url = pho['photo']['thumb_url']
+      photo.small_url = pho['photo']['small_url']
+      photo.medium_url = pho['photo']['medium_url']
+      photo.large_url = pho['photo']['large_url']
+                                                                     #photo.original_url = pho['photo']['original_url']
+      photo.created_at = pho['photo']['created_at']
+      photo.updated_at = pho['photo']['updated_at']
+      photo.native_page_url = pho['photo']['native_page_url']
+      photo.native_username = pho['photo']['native_username']
+      photo.native_realname = pho['photo']['native_realname']
+      photo.license = pho['photo']['license']
+      photo.type = photo_type(pho['photo']['thumb_url'])
+      photos << photo
+    end
+    photos
+  end
 end
