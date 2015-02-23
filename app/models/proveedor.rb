406199c703a20f0ea2ca2d0dc9de5620c24317a9
calonso-conabio <calonso@conabio.gob.mx>
2015-02-23 15:02:09 -0600

    conservando los kml

--- app/models/proveedor.rb
+++ app/models/proveedor.rb
@@ -122,7 +122,6 @@ class Proveedor < ActiveRecord::Base
     File.open(ruta_kml, 'w+') { |file| file.write(snib_kml) }
     system "zip #{ruta.join('registros')} #{ruta_kml}"
     ruta_zip = ruta.join('registros.zip')
-    File.delete(ruta_kml) if File.exists?(ruta_kml)
     rename = File.rename(ruta_zip, ruta.join('registros.kmz'))
     rename == 0
   end
@@ -134,7 +133,6 @@ class Proveedor < ActiveRecord::Base
     File.open(ruta_kml, 'w+') { |file| file.write(naturalista_kml) }
     system "zip #{ruta.join('observaciones')} #{ruta_kml}"
     ruta_zip = ruta.join('observaciones.zip')
-    File.delete(ruta_kml) if File.exists?(ruta_kml)
     rename = File.rename(ruta_zip, ruta.join('observaciones.kmz'))
     rename == 0
   end
