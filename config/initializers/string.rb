d44d60f3a411fd6190f53dcf1ef276d5b8d9fc98
calonso-conabio <calonso@conabio.gob.mx>
2015-02-27 15:42:43 -0600

    actualiza la el metodo limpia y limpiar de string

--- config/initializers/string.rb
+++ config/initializers/string.rb
@@ -1,11 +1,19 @@
 class String
+  # Pone en mayusculas o minusculas los acentos
   def humanizar
     self.humanize.mb_chars.capitalize.to_s
   end
 
+  # Quita simbolos raros y quita los terminos con punto que estan abajo de especies y subgenero
   def limpiar
     return self unless self.present?
-    self.gsub(/(\r\n|\r|\n)/, '').gsub('"', '\"').gsub("\t", ' ').gsub(/\([^()]*\)/, ' ').gsub(/( subsp\.| f\. | var\.| subf\.| subvar\.| sect\.)/, ' ').strip.gsub(/\s+/,' ')
+    self.limpia.gsub(/\([^()]*\)/, ' ').gsub(/( subsp\.| f\. | var\.| subf\.| subvar\.| sect\.)/, ' ').strip.gsub(/\s+/,' ')
+  end
+
+  # Quita simbolos raros
+  def limpia
+    return self unless self.present?
+    self.gsub(/(\r\n|\r|\n)/, '').gsub('"', '\"').gsub("\t", ' ').strip.gsub(/\s+/,' ')
   end
 end
 
