c46d9d4c43e0b231d4f18ada668515c4b926d8fe
Alberto González <albertoglezba@gmail.com>
2015-04-01 10:58:50 -0600

    Modificación del script que levanta

--- levantaServerDevelopment.sh
+++ levantaServerDevelopment.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-redis-server > bitacoraScript &
+redis-server > log/bitacoraScript.log &
 sleep 10
-soulmate-web --foreground --no-launch --redis=redis://localhoscd CONABIO/buscador/ >> bitacoraScript &
+soulmate-web --foreground --no-launch --redis=redis://localhoscd CONABIO/buscador/ >> log/bitacoraScript.log &
 rails s -p 4000
