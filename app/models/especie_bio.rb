b0edf44d6c1a61f35085499ed9834ee2d9a39c45
calonso-conabio <calonso@conabio.gob.mx>
2015-05-08 13:01:45 -0500

    quita nombre_comun_principal de EspecieBio

--- app/models/especie_bio.rb
+++ app/models/especie_bio.rb
@@ -216,9 +216,6 @@ class EspecieBio < ActiveRecord::Base
       }
   }
 
-  # Lenguas aceptadas de NaturaLista
-  LENGUAS_ACEPTADAS = %w(spanish espanol_mexico huasteco maya maya_peninsular mayan_languages mazateco mixteco mixteco_de_yoloxochitl totonaco otomi nahuatl zapoteco english)
-
   # Override assignment method provided by has_many to ensure that all
   # callbacks on photos and taxon_photos get called, including after_destroy
   def photos=(new_photos)
@@ -483,54 +480,6 @@ class EspecieBio < ActiveRecord::Base
 
     # Para quitar los espacios adicionales
     self.nombre_cientifico = nombre_cientifico.strip.gsub(/\s+/,' ')
-  end
-
-  def pon_nombre_comun_principal(base)
-    con_espaniol = false
-
-    # Verifica el nombre en catalogos
-    nombres_comunes.each do |nc|
-      if !con_espaniol && nc.lengua == 'Español'
-        self.nombre_comun_principal = nc.nombre_comun.humanizar
-        con_espaniol = true
-      elsif !con_espaniol && nc.lengua == 'Inglés'
-        self.nombre_comun_principal = nc.nombre_comun.humanizar
-      elsif !con_espaniol
-        self.nombre_comun_principal = nc.nombre_comun.humanizar
-      end
-    end
-
-    # Si no tiene nombre comun en catalogos tratare de ponerle uno de NaturaLista
-    if nombre_comun_principal.blank?
-      id_vista = Bases.id_original_a_id_en_vista(id)
-      Bases.conecta_a Rails.env
-
-      begin
-        taxon = Especie.find(id_vista)
-      rescue
-        Bases.conecta_a(base)
-        return
-      end
-
-      return unless prov = taxon.proveedor
-      return unless prov.naturalista_info.present?
-
-      datos = eval(prov.naturalista_info)
-      datos = datos.first if datos.is_a?(Array)
-      default_name = datos['default_name']
-
-      return unless default_name.present?
-      return unless default_name['is_valid']
-      return unless default_name['name'].present?
-      return unless default_name['lexicon'].present?
-
-      lexicon = I18n.transliterate(default_name['lexicon']).gsub(' ','_').downcase
-      return unless LENGUAS_ACEPTADAS.include?(lexicon)
-      nombre_comun_principal = default_name['name']
-
-      Bases.conecta_a(base)
-      self.nombre_comun_principal = nombre_comun_principal.humanizar
-    end
   end
 
   def completa_redis?
