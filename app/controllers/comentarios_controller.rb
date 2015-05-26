24c065584141709a1f1b7ec39655fc8e722b841a
calonso-conabio <calonso@conabio.gob.mx>
2015-05-26 17:43:58 -0500

    comentarios parte 2

--- app/controllers/comentarios_controller.rb
+++ app/controllers/comentarios_controller.rb
@@ -20,7 +20,7 @@ class ComentariosController < ApplicationController
 
   # GET /comentarios/new
   def new
-    @comentario = Comentario.new
+    @comentario = Comentario.new(especie_id: params[:especie_id])
   end
 
   # GET /comentarios/1/edit
