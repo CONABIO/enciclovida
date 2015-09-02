39459ad4e9b0d3cd7611ef85a42135aea9a715b1
Alberto González <albertoglezba@gmail.com>
2015-09-01 20:04:32 -0500

    borrado de los indices q hacia q fallara en produccion

--- app/indices/nombre_comun_index.rb
+++
@@ -1,7 +0,0 @@
-# app/indices/article_index.rb
-ThinkingSphinx::Index.define :nombre_comun, :with => :active_record do
-  indexes nombre_comun, :sortable => true
-  #indexes "LOWER(nombre_comun)", :as => :nombre_comun, :sortable => true
-  #has lengua, created_at, updated_at
-  #indexes where "nombre_comun LIKE '%gal%'"
-end
