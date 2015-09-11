8b033c749e120a6c375cd19bab83cc83e7ecfd48
calonso-conabio <calonso@conabio.gob.mx>
2015-09-10 19:17:36 -0500

    funcion para todos los ambientes

--- app/models/catalogo.rb
+++ app/models/catalogo.rb
@@ -6,6 +6,10 @@ class Catalogo < ActiveRecord::Base
   has_many :especies_catalogos, :class_name => 'EspecieCatalogo'
   #has_one :especie, :through => :especies_catalogos, :class_name => 'EspecieCatalogo', :foreign_key => 'catalogo_id'
 
+  IUCN_QUITAR_EN_FICHA = ['Riesgo bajo (LR): Dependiente de conservación (cd)', 'No evaluado (NE)', 'Datos insuficientes (DD)',
+  'Riesgo bajo (LR): Preocupación menor (lc)', 'Riesgo bajo (LR): Casi amenazado (nt)']
+  AMBIENTE_EQUIV_MARINO = ['Nerítico', 'Nerítico y oceánico', 'Oceánico']
+
   # Saco el nombre de la categoria de riesgo o comercio ya que al unir los catalogos, los nombres aveces no coinciden
   def nom_cites_iucn
     if nivel1 == 4 && nivel2 > 0 && nivel3 > 0   #se asegura que el valor pertenece a la nom, iucn o cites
@@ -21,7 +25,7 @@ class Catalogo < ActiveRecord::Base
 
   # Saco el nombre del ambiente ya que al unir los catalogos, los nombres aveces no coinciden
   def ambiente
-    if nivel1 == 2 && nivel2 > 0 && nivel3 > 0   #se asegura que el valor pertenece al ambiente
+    if nivel1 == 2 && nivel2 == 6 && nivel3 > 0   #se asegura que el valor pertenece al ambiente
       limites = Bases.limites(id)
       id_inferior = limites[:limite_inferior]
       id_superior = limites[:limite_superior]
@@ -33,12 +37,8 @@ class Catalogo < ActiveRecord::Base
   end
 
   def self.ambiente_todos
-    limites = Bases.limites(id)
-    id_inferior = limites[:limite_inferior]
-    id_superior = limites[:limite_superior]
-    ambiente = Catalogo.where(:nivel1 => 2, :nivel2 => 0, :nivel3 => 0).where(:id => id_inferior..id_superior)
-    puts '+++++++++++++++++++++++'+ambiente.inspect
-    ambiente.inspect
+    ambiente = Catalogo.where(:nivel1 => 2, :nivel2 => 6).where('nivel3 > 0').map(&:descripcion).uniq
+    ambiente.delete_if{|a| AMBIENTE_EQUIV_MARINO.include?(a)}
   end
 
   def self.nom_cites_iucn_todos
