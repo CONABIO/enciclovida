bd364a30ca6981d80f429e3f4c631ef3b79389c5
calonso-conabio <calonso@conabio.gob.mx>
2016-04-18 12:40:31 -0500

    geodatos - desplegando el mapa sin un layout

---
+++ app/assets/javascripts/geodatos/geodatos_todos.js
@@ -0,0 +1,6 @@
+//= require "geodatos/leaflet.markercluster-src.js"
+//= require "geodatos/jstree.min.js"
+//= require "geodatos/L.TileLayer.BetterWMS.js"
+//= require "geodatos/toastr.js"
+//= require "geodatos/geodatos.js"
+//= require "geodatos/easy-button.js"
