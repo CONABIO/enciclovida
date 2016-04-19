b0533124c421a1397119c5a4e0b8331b22811094
calonso-conabio <calonso@conabio.gob.mx>
2016-04-18 22:12:42 -0500

    el arbol de nodo ahora se muestra en la ficha

--- app/controllers/especies_controller.rb
+++ app/controllers/especies_controller.rb
@@ -2,8 +2,8 @@ class EspeciesController < ApplicationController
 
   skip_before_filter :set_locale, only: [:kmz, :kmz_naturalista, :create, :update, :edit_photos]
   before_action :set_especie, only: [:show, :edit, :update, :destroy, :edit_photos, :update_photos, :describe,
-                                     :datos_principales, :kmz, :kmz_naturalista, :cat_tax_asociadas, :descripcion_catalogos, :arbol]
-  before_action :only => [:arbol_inicial, :json_d3, :nodo_json_d3] do
+                                     :datos_principales, :kmz, :kmz_naturalista, :cat_tax_asociadas, :descripcion_catalogos]
+  before_action :only => [:arbol, :arbol_inicial, :json_d3, :nodo_json_d3] do
     set_especie(true)
   end
 
