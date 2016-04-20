b3800836905687bb9db513e5b64e258d5d8fb2e3
calonso-conabio <calonso@conabio.gob.mx>
2016-04-20 16:06:36 -0500

    probando añadir nuevas capas con kml

--- app/assets/javascripts/geodatos/geodatos_todos.js
+++ app/assets/javascripts/geodatos/geodatos_todos.js
@@ -1,5 +1,6 @@
 //= require "geodatos/leaflet.markercluster-src.js"
 //= require "geodatos/jstree.min.js"
+//= require "geodatos/KML.js"
 //= require "geodatos/L.TileLayer.BetterWMS.js"
 //= require "geodatos/toastr.js"
 //= require "geodatos/geodatos.js"
