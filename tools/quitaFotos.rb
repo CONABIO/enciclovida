3bf46198b331c6f707ae89f1fb40000757d4290d
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2016-04-29 12:46:48 -0500

    scipcito para quitar links rotos

--- tools/quitaFotos.rb
+++ tools/quitaFotos.rb
@@ -1,3 +1,4 @@
+#Para correr en ecolibri, o donde se tenga montado mount -t cifs "//bdi/FotoWare Assets" /fotosBDI -o user=fotoweb
 start_time = Time.now
 puts "Inicie en #{start_time} " 
 
@@ -6,10 +7,11 @@ Metadato.all.each do |f|
   print "\nMetadato.id = #{f.id.to_s} "
   f_url =  URI.encode("http://bdi.conabio.gob.mx:5050"+f.path[29..-1])
   print f_url
-next if (Net::HTTP.get_response(URI(f_url)).class != "Net::HTTPNotFound")
+next if (Net::HTTP.get_response(URI(f_url)).class.inspect != "Net::HTTPNotFound")
 puts " <----- Este lo borro (#{f_url})"
-Photo.find_by_native_photo_id(f.id.to_s).destroy
-f.destroy
+  p = Photo.find_by_native_photo_id(f.id.to_s)
+  p.destroy if p
+  f.destroy
 end
 
-puts "Termino en #{Time.now - start_time} seg" if OPTS[:debug]
+puts "Termino en #{Time.now - start_time} seg"
