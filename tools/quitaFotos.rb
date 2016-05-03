1a41ad5cfdd091c0a6db601744cc208fdc4468d6
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2016-05-02 21:20:16 -0500

    script de quitaFotos corregido

--- tools/quitaFotos.rb
+++ tools/quitaFotos.rb
@@ -3,15 +3,14 @@ start_time = Time.now
 puts "Inicie en #{start_time} " 
 
 
-Metadato.all.each do |f|
-  print "\nMetadato.id = #{f.id.to_s} "
-  f_url =  URI.encode("http://bdi.conabio.gob.mx:5050"+f.path[29..-1])
-  print f_url
-next if (Net::HTTP.get_response(URI(f_url)).class.inspect != "Net::HTTPNotFound")
-puts " <----- Este lo borro (#{f_url})"
-  p = Photo.find_by_native_photo_id(f.id.to_s)
+Metadato.all.each do |m|
+  print "\nMetadato.id = #{m.id.to_s} "
+  print m.path.inspect
+  next if File.file?(f.path.inspect)
+puts " <----- Este lo borro (#{m_url})"
+  p = Photo.find_by_native_photo_id(m.id.to_s)
   p.destroy if p
-  f.destroy
+  m.destroy
 end
 
 puts "Termino en #{Time.now - start_time} seg"
