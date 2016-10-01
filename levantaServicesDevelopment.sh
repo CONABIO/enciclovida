9fb8bb6e7703292991dc38bc426565432665fe31
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2016-09-30 22:35:51 -0500

    idem

--- levantaServicesDevelopment.sh
+++ levantaServicesDevelopment.sh
@@ -4,4 +4,4 @@ redis-server > log/bitacoraScript.log &
 sleep 10
 soulmate-web --foreground --no-launch --redis=redis://localhost >> log/bitacoraScript.log &
 rails runner "eval(File.read 'levantaBlurrily.rb')" &
-rails s -p 4000
+
