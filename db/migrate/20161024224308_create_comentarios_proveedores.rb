46603984674bf097b4879084aa048205ac9fe472
calonso-conabio <calonso@conabio.gob.mx>
2016-10-25 20:47:41 -0500

    desplegando el enlace del registro en el panel admin de comentarios

--- db/migrate/20161024224308_create_comentarios_proveedores.rb
+++ db/migrate/20161024224308_create_comentarios_proveedores.rb
@@ -1,7 +1,7 @@
 class CreateComentariosProveedores < ActiveRecord::Migration
   def change
     create_table :comentarios_proveedores do |t|
-      t.integer :comentario_id, null: false
+      t.string :comentario_id, limit: 10, null: false
       t.string :proveedor_id, null: false
       t.timestamps
     end
