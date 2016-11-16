7435f1d3c652a17336d2f0e9402260e6a754779b
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2016-11-16 13:41:16 -0600

    corrigiendo migracion

--- db/migrate/20161116023051_create_roles_categorias_contenido.rb
+++ db/migrate/20161116023051_create_roles_categorias_contenido.rb
@@ -1,7 +1,7 @@
 class CreateRolesCategoriasContenido < ActiveRecord::Migration
   def change
     create_table :roles_categorias_contenido do |t|
-      t.integer :categoria_contenido_id
+      t.integer :categorias_contenido_id
       t.integer :rol_id
 
       t.timestamps
