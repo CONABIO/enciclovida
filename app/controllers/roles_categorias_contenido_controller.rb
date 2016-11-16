2384cce660664fd772ba761a813937ee01906743
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2016-11-16 14:56:27 -0600

    cambio en el show modelo y controlador para RolCategoriaSContenido

--- app/controllers/roles_categorias_contenido_controller.rb
+++ app/controllers/roles_categorias_contenido_controller.rb
@@ -1,10 +1,16 @@
 class RolesCategoriasContenidoController < ApplicationController
   before_action :set_rol_categorias_contenido, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_usuario!
+  before_action { tiene_permiso?(2) } # Minimo administrador
+  before_action do
+    Rails.application.reload_routes!
+    @no_render_busqueda_basica = true
+  end
 
   # GET /roles_categorias_contenido
   # GET /roles_categorias_contenido.json
   def index
-    @roles_categorias_contenido = RolCategoriasContenido.all
+    @roles_categorias_contenido = RolCategoriasContenido.join_roles_categorias_contenidos.order(:rol_id).load
   end
 
   # GET /roles_categorias_contenido/1
@@ -28,7 +34,7 @@ class RolesCategoriasContenidoController < ApplicationController
 
     respond_to do |format|
       if @rol_categorias_contenido.save
-        format.html { redirect_to @rol_categorias_contenido, notice: 'Rol categorias contenido was successfully created.' }
+        format.html { redirect_to @rol_categorias_contenido, notice: 'Rol - CategoriasContenido se creo satisfactoriamente.' }
         format.json { render action: 'show', status: :created, location: @rol_categorias_contenido }
       else
         format.html { render action: 'new' }
@@ -42,7 +48,7 @@ class RolesCategoriasContenidoController < ApplicationController
   def update
     respond_to do |format|
       if @rol_categorias_contenido.update(rol_categorias_contenido_params)
-        format.html { redirect_to @rol_categorias_contenido, notice: 'Rol categorias contenido was successfully updated.' }
+        format.html { redirect_to @rol_categorias_contenido, notice: 'Rol - CategoriasContenido se actualizó satisfactoriamente.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -64,7 +70,7 @@ class RolesCategoriasContenidoController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_rol_categorias_contenido
-      @rol_categorias_contenido = RolCategoriasContenido.find(params[:id])
+      @rol_categorias_contenido = RolCategoriasContenido.join_roles_categorias_contenidos.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/rol_categorias_contenido.rb
+++ app/models/rol_categorias_contenido.rb
@@ -3,7 +3,7 @@ class RolCategoriasContenido < ActiveRecord::Base
   belongs_to :rol
 
   scope :join_roles,-> { joins('JOIN roles on rol_id = roles.id') }
-  scope :join_categorias_contenido,-> { joins('JOIN categorias_contenido on categoria_contenido_id = categorias_contenido.id') }
-  scope :select_para_joins, -> { select("roles_categorias_contenido.id, categoria_contenido_id, categorias_contenido.nombre, rol_id, roles.nombre_rol")}
-  scope :join_categorias_contenido_rol,-> { select_para_joins.join_roles.join_categorias_contenido }
+  scope :join_categorias_contenido,-> { joins('JOIN categorias_contenido on categorias_contenido_id = categorias_contenido.id') }
+  scope :select_para_joins, -> { select("roles_categorias_contenido.id, categorias_contenido_id, categorias_contenido.nombre, rol_id, roles.nombre_rol")}
+  scope :join_roles_categorias_contenidos,-> { select_para_joins.join_roles.join_categorias_contenido }
 end
