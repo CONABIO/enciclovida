2529b9ff7c943f668960a9026d9e9a632f328b07
calonso-conabio <calonso@conabio.gob.mx>
2017-04-26 13:52:37 -0500

    archivos iniciales para la ubicacion

---
+++ app/controllers/ubicaciones_controller.rb
@@ -0,0 +1,25 @@
+class UbicacionesController < ApplicationController
+
+  skip_before_filter :verify_authenticity_token, :set_locale
+  before_action :set_ubicacion, only: []
+  before_action :authenticate_usuario!, :except => :create
+
+  # Registros con un radio alreadedor de tu ubicación
+  def ubicacion
+
+  end
+
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_ubicacion
+    @ubicacion = Metadato.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def ubicacion_params
+    params.require(:ubicacion).permit(:path, :object_name, :artist, :copyright)
+  end
+end
+
