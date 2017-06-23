76f0e5d7c6da7d1fed9c729f733327be7fcde880
calonso-conabio <calonso@conabio.gob.mx>
2017-06-22 20:31:55 -0500

    pasandole solo el numero de paginado al servicio de bdi

--- lib/bdi_service.rb
+++ lib/bdi_service.rb
@@ -1,20 +1,18 @@
 class BDIService
 
-  def dameFotos(nombre_cientifico, url=nil)
-    bdi = 'http://bdi.conabio.gob.mx'
+  def dameFotos(nombre_cientifico, p=nil)
+    bdi = CONFIG.bdi_imagenes
     fotos = []
 
-    if url.nil?
-      url = URI.parse("http://bdi.conabio.gob.mx/fotoweb/archives/5000-Banco%20de%20Im%C3%A1genes/?528='#{nombre_cientifico.gsub(" ","+")}'")
-    else
-      url = URI.parse(url)
-    end
+    url = "#{bdi}/fotoweb/archives/5000-Banco%20de%20Im%C3%A1genes/?528='#{nombre_cientifico.gsub(" ","+")}'"
+    url << "&p=#{p-1}" if p
+    uri = URI.parse(url)
 
-    req = Net::HTTP::Get.new(url.to_s)
+    req = Net::HTTP::Get.new(uri.to_s)
     req['Accept'] = 'application/vnd.fotoware.assetlist+json'
 
     begin
-      res = Net::HTTP.start(url.host, url.port) {|http| http.request(req) }
+      res = Net::HTTP.start(uri.host, uri.port) {|http| http.request(req) }
       jres = JSON.parse(res.body)
     rescue => e
       {:estatus => 'error', :msg => e}
@@ -25,9 +23,10 @@ class BDIService
     end
 
     if jres['paging'].present? && jres['paging']['next'].present?
-      {:estatus => 'OK', :siguiente => bdi+jres['paging']['next'], :ultima => bdi+jres['paging']['last'], :fotos => fotos}
+      ultima = jres['paging']['last'].split('&p=').last.to_i + 1
+      {:estatus => 'OK', :ultima => ultima, :fotos => fotos}
     else
-      {:estatus => 'OK', :siguiente => '', :fotos => fotos}
+      {:estatus => 'OK', :ultima => nil, :fotos => fotos}
     end
   end
 end
