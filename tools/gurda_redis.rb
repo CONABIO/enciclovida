b31a091df902edcc1506783761277958d9fab578
calonso-conabio <calonso@conabio.gob.mx>
2017-06-26 15:32:20 -0500

    script para correr el redis desde un inicio

---
+++ tools/gurda_redis.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+require 'trollop'
+
+OPTS = Trollop::options do
+  banner <<-EOS
+Exporta todas los nombres cientificos, nombres comunes de catalogos y nombres comunes de naturalista a redis,
+adicionalmente guarda la foto principal y el nombre comun principal
+
+
+*** Este script solo es necesario correrlo una vez, ya que la ficha actualiza el taxon
+
+Usage:
+
+  rails r tools/guarda_redis.rb -d
+
+where [options] are:
+  EOS
+  opt :debug, 'Print debug statements', :type => :boolean, :short => '-d'
+end
+
+
+def guarda_redis
+  puts 'Procesando los nombres cientificos...' if OPTS[:debug]
+
+  Especie.find_each do |t|
+    #Especie.limit(100).each do |t|
+    puts "#{t.id}-#{t.nombre_cientifico}" if OPTS[:debug]
+    t.guarda_redis
+  end
+end
+
+
+start_time = Time.now
+
+guarda_redis
+
+puts "Termino en #{Time.now - start_time} seg" if OPTS[:debug]
