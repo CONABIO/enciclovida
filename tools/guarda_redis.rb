3995572324e925fa657c99637152abb0bed6234f
calonso-conabio <calonso@conabio.gob.mx>
2017-06-27 19:14:06 -0500

    cambios en el script que guarda el redis

--- tools/guarda_redis.rb
+++ tools/guarda_redis.rb
@@ -21,8 +21,13 @@ end
 
 def guarda_redis
   puts 'Procesando los nombres cientificos...' if OPTS[:debug]
+  ultima_corrida = false
 
   Especie.find_each do |t|
+    # Para saltarse a algun taxon en especifico, por si se quedo truco el redis
+    ultima_corrida = true if t.id == 6001318
+    next unless ultima_corrida
+
     #Especie.limit(100).each do |t|
     puts "#{t.id}-#{t.nombre_cientifico}" if OPTS[:debug]
     t.guarda_redis
