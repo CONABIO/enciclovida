0c6ea2fc924999f293ef0b32c1cb4f9f2e3ec0bb
calonso-conabio <calonso@conabio.gob.mx>
2017-07-26 22:55:45 -0500

    los caches ahora con mas controlo en el config.yml

--- app/models/especie.rb
+++ app/models/especie.rb
@@ -454,7 +454,7 @@ Dalbergia_ruddae Dalbergia_stevensonii Dalbergia_cubilquitzensis)
   # Es un metodo que no depende del la tabla proveedor, puesto que consulta naturalista sin el ID
   def ficha_naturalista_por_nombre
     return {estatus: 'error', msg: 'No hay resultados'} if existe_cache?('ficha_naturalista')
-    especie.escribe_cache('ficha_naturalista', 1.day) if Rails.env.production?
+    escribe_cache('ficha_naturalista', eval(CONFIG.cache.ficha_naturalista)) if Rails.env.production?
 
     begin
       respuesta = RestClient.get "#{CONFIG.naturalista_url}/taxa/search.json?q=#{URI.escape(nombre_cientifico.limpiar.limpia)}"
