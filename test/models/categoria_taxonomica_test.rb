0953c110c78cbdeda991539884cc40bef4fa279a
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2017-08-10 14:16:20 -0500

    borrando archivos innecesarios... por el momento

--- test/models/categoria_taxonomica_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CategoriaTaxonomicaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/rol_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RolTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
