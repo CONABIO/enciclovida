c6ae4247c74a4eab2fc5b143d979ece154258b23
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2017-09-14 20:14:58 -0500

    al parecer el ws de github ya funciona como debe

--- lib/github_service.rb
+++ lib/github_service.rb
@@ -1,32 +1,10 @@
 class GithubService
 
-  def damePulls(numero=nil)
-    @github = CONFIG.github.api
+  def getIssues(type='issues')
 
+    url = CONFIG.github.api << "/#{type}?state=closed"
+    url = url << '&labels=new feature' if type == 'issues'
 
-    url = "#{@github}/pulls?state=closed"
-    url_escape = URI.escape(url)
-    uri = URI.parse(url_escape)
-    req = Net::HTTP::Get.new(uri.to_s)
-    req['Accept'] = 'application/vnd.github.v3+json'
-
-    begin
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = (uri.scheme == "https")
-      res = http.request(req)
-
-      jres = JSON.parse(res.body)
-      jres[0..4] if jres.any?
-    rescue => e
-      nil
-    end
-  end
-
-  def dameIssuesNF
-    @github = CONFIG.github.api
-
-
-    url = "#{@github}/issues?state=all&label=new%20feature"
     url_escape = URI.escape(url)
     uri = URI.parse(url_escape)
     req = Net::HTTP::Get.new(uri.to_s)
