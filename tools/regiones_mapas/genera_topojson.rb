3795fecc3f0c50dd29d26fb7d7e3b4c2abe950c2
calonso-conabio <calonso@conabio.gob.mx>
2017-10-26 23:47:37 -0500

    Cambiando los layers por capas en d3

--- tools/regiones_mapas/genera_topojson.rb
+++ tools/regiones_mapas/genera_topojson.rb
@@ -16,20 +16,23 @@ end
 def guarda_topojson
   puts 'Generando los topojson' if OPTS[:debug]
 
-  regiones = %w(estado municipio anp ecorregion)
+  #regiones = %w(estado municipio anp ecorregion)
+  regiones = %w(estado)
   ruta = Rails.root.join('public', 'topojson')
   Dir.mkdir(ruta) unless File.exists?(ruta)
 
   regiones.each do |region|  # Itera sobre los 4 tipos de region
     puts "\tGenerando con el tipo de región: #{region}" if OPTS[:debug]
     topo = GeoATopo.new
+    geojson_region = {type: 'FeatureCollection', features: []}  # Para todos loes estados o municipios juntos
 
     region.camelize.constantize.select('ST_AsGeoJSON(the_geom) AS geojson').campos_min.all.each do |reg|
       puts "\t\tGenerando la región: #{reg.nombre_region}" if OPTS[:debug]
 
-      geojson = {"type" => "FeatureCollection", "features" => []}
-      feature = {"type" =>"Feature", "properties" => {"nombre_region" => reg.nombre_region}, "geometry" => JSON.parse(reg.geojson)}
-      geojson["features"] << feature
+      geojson = {type: 'FeatureCollection', features: []}
+      feature = {type: 'Feature', properties:{nombre_region: reg.nombre_region}, geometry: JSON.parse(reg.geojson)}
+      geojson[:features] << feature
+      geojson_region[:features] << feature
       topojson = topo.dame_topojson(geojson.to_json)
 
       archivo = if region == 'municipio'
@@ -39,8 +42,10 @@ def guarda_topojson
                 end
 
       File.write(archivo, topojson.to_json)
-      
     end  # End cada region each
+
+    topojson = topo.dame_topojson(geojson_region.to_json)
+    File.write(ruta.join("#{region}.json"), topojson.to_json)
   end  # End tipos regiones each
 end
 
