ba51728845450f265367dc765ba6d55f7af65380
calonso-conabio <calonso@conabio.gob.mx>
2017-11-08 17:39:17 -0600

    generando la  division municipal por estado

--- tools/regiones_mapas/genera_topojson.rb
+++ tools/regiones_mapas/genera_topojson.rb
@@ -13,11 +13,12 @@ where [options] are:
   opt :debug, 'Print debug statements', :type => :boolean, :short => '-d'
 end
 
-def guarda_topojson
-  puts 'Generando los topojson' if OPTS[:debug]
+# Guarda el topo json de cada region y las divisiones politicas
+def topojson_por_region
+  puts 'Generando los topojson por region' if OPTS[:debug]
 
-  #regiones = %w(estado municipio anp ecorregion)
-  regiones = %w(municipio)
+  regiones = %w(estado municipio anp ecorregion)
+  #regiones = %w(municipio)
   ruta = Rails.root.join('public', 'topojson')
   Dir.mkdir(ruta) unless File.exists?(ruta)
 
@@ -60,14 +61,48 @@ def guarda_topojson
     end  # End cada region each
 
     File.write(ruta.join("#{region}_geo.json"), geojson_region.to_json)
-    topojson = topo.dame_topojson(geojson_region.to_json)
-    File.write(ruta.join("#{region}.json"), topojson.to_json)
+    if region == 'estado'
+      topojson = topo.dame_topojson(geojson_region.to_json)
+      File.write(ruta.join("#{region}.json"), topojson.to_json)
+    end
+
   end  # End tipos regiones each
 end
 
+# Carga una division municipal por estado
+def topojson_municipios_por_estado
+  puts 'Generando los municipios por estado' if OPTS[:debug]
+  topo = GeoATopo.new
+
+  Estado.campos_min.all.each do |e|
+    puts "Generando con estado: #{e.nombre_region}" if OPTS[:debug]
+    geojson = {type: 'FeatureCollection', features: []}  # Para todos loes estados o municipios juntos
+    estado_id = Estado::CORRESPONDENCIA[e.region_id]
+    estado_nombre = I18n.t("estados.#{e.nombre_region.estandariza}")
+
+    Municipio.select('ST_AsGeoJSON(the_geom) AS geojson').campos_min.where(cve_ent: estado_id).each do |m|
+      puts "\tGenerando con municipio: #{m.nombre_region}" if OPTS[:debug]
+      feature = {type: 'Feature', properties:{}}
+      feature[:properties][:region_id] = m.region_id
+      feature[:properties][:nombre_region] = "#{m.nombre_region}, #{estado_nombre}, MX"
+      feature[:properties][:parent_id] = m.parent_id
+      feature[:geometry] = JSON.parse(m.geojson)
+      geojson[:features] << feature
+    end
+
+    topojson = topo.dame_topojson(geojson.to_json)
+
+    ruta = Rails.root.join('public', 'topojson')
+    Dir.mkdir(ruta) unless File.exists?(ruta)
+    archivo = ruta.join("estado_#{e.region_id}_division_municipal.json")
+
+    File.write(archivo, topojson.to_json)
+  end
+end
 
 start_time = Time.now
 
-guarda_topojson
+#topojson_por_region
+topojson_municipios_por_estado
 
 puts "Termino en #{Time.now - start_time} seg" if OPTS[:debug]
