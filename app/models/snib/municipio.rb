c6edda229a7ad7680ab87089bf3018030e459412
calonso-conabio <calonso@conabio.gob.mx>
2017-11-17 16:39:40 -0600

    cargando los registros exactos por municipio

--- app/models/snib/municipio.rb
+++ app/models/snib/municipio.rb
@@ -2,7 +2,7 @@ class Municipio < ActiveRecord::Base
   establish_connection(:snib)
   self.primary_key = 'munid'
 
-  scope :campos_min, -> { select('cve_mun AS region_id, cve_ent AS parent_id, municipio AS nombre_region').order(municipio: :asc) }
+  scope :campos_min, -> { select('cve_mun AS region_id, cve_ent AS parent_id, municipio AS nombre_region, munid AS region_id_se').order(municipio: :asc) }
   scope :campos_geom, -> { select('ST_AsGeoJSON(the_geom) AS geojson, st_x(st_centroid(the_geom)) AS long, st_y(st_centroid(the_geom)) AS lat') }
   scope :geojson, ->(region_id, parent_id) { select('ST_AsGeoJSON(the_geom) AS geojson').where(cve_mun: region_id, cve_ent: parent_id) }
 end
