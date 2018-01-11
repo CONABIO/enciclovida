cbc8f1075f347b738729c18b02686f16f17c2f20
calonso-conabio <calonso@conabio.gob.mx>
2018-01-10 21:38:34 -0600

    modelo y sql para crear base de peces 2018

---
+++ db/peces/peces2018.sql
@@ -0,0 +1,177 @@
+-- MySQL dump 10.13  Distrib 5.6.28, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: peces2018
+-- ------------------------------------------------------
+-- Server version	5.6.28-1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `criterios`
+--
+
+DROP TABLE IF EXISTS `criterios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `criterios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_criterio` varchar(255) NOT NULL,
+  `valor` tinyint(2) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Contiene los criterios principales para el semaforo: nom, iucn, selectiva, nacional, CNP (por estado), veda (por tipo de veda y mes), pesqeria en sustentabilidad';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `criterios`
+--
+
+LOCK TABLES `criterios` WRITE;
+/*!40000 ALTER TABLE `criterios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `criterios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `especies`
+--
+
+DROP TABLE IF EXISTS `especies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `especies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `especies`
+--
+
+LOCK TABLES `especies` WRITE;
+/*!40000 ALTER TABLE `especies` DISABLE KEYS */;
+/*!40000 ALTER TABLE `especies` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `peces`
+--
+
+DROP TABLE IF EXISTS `peces`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `peces` (
+  `especie_id` int(11) NOT NULL,
+  `tipo_arte` varchar(255) DEFAULT NULL,
+  `destribución` varchar(255) DEFAULT NULL,
+  `valor_total_promedio` tinyint(2) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`especie_id`),
+  CONSTRAINT `FK_IDES4` FOREIGN KEY (`especie_id`) REFERENCES `especies` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `peces`
+--
+
+LOCK TABLES `peces` WRITE;
+/*!40000 ALTER TABLE `peces` DISABLE KEYS */;
+/*!40000 ALTER TABLE `peces` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `peces_criterios`
+--
+
+DROP TABLE IF EXISTS `peces_criterios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `peces_criterios` (
+  `especie_id` int(11) NOT NULL,
+  `criterio_id` int(11) NOT NULL,
+  PRIMARY KEY (`criterio_id`,`especie_id`),
+  KEY `fk_criterios_categorias_criterios1_idx` (`criterio_id`),
+  KEY `fk_peces_criterios_peces1_idx` (`especie_id`),
+  CONSTRAINT `fk_criterios_categorias_criterios1` FOREIGN KEY (`criterio_id`) REFERENCES `criterios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_peces_criterios_peces1` FOREIGN KEY (`especie_id`) REFERENCES `peces` (`especie_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `peces_criterios`
+--
+
+LOCK TABLES `peces_criterios` WRITE;
+/*!40000 ALTER TABLE `peces_criterios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `peces_criterios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `peces_propiedades`
+--
+
+DROP TABLE IF EXISTS `peces_propiedades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `peces_propiedades` (
+  `especie_id` int(11) NOT NULL,
+  `propiedad_id` int(11) NOT NULL,
+  PRIMARY KEY (`especie_id`,`propiedad_id`),
+  KEY `fk_grupos_peces1_idx` (`especie_id`),
+  KEY `fk_peces_grupos_grupos1_idx` (`propiedad_id`),
+  CONSTRAINT `fk_grupos_peces1` FOREIGN KEY (`especie_id`) REFERENCES `peces` (`especie_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_peces_grupos_grupos1` FOREIGN KEY (`propiedad_id`) REFERENCES `propiedades` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `peces_propiedades`
+--
+
+LOCK TABLES `peces_propiedades` WRITE;
+/*!40000 ALTER TABLE `peces_propiedades` DISABLE KEYS */;
+/*!40000 ALTER TABLE `peces_propiedades` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `propiedades`
+--
+
+DROP TABLE IF EXISTS `propiedades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `propiedades` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_propiedad` varchar(255) NOT NULL,
+  `tipo_propiedad` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tipo de propiedades como: distribucion, arte de pesca, grupos y demas que apliquen';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `propiedades`
+--
+
+LOCK TABLES `propiedades` WRITE;
+/*!40000 ALTER TABLE `propiedades` DISABLE KEYS */;
+/*!40000 ALTER TABLE `propiedades` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-01-10 21:37:37
