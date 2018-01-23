cbf514ef69254ab69e09b8c949a138a9df68ddd8
calonso-conabio <calonso@conabio.gob.mx>
2018-01-23 15:54:23 -0600

    quitando los parametros repetidos de la busqueda avanzada

--- app/assets/javascripts/busquedas.js
+++ app/assets/javascripts/busquedas.js
@@ -154,46 +154,5 @@ $(document).ready(function()
         if (url == "") return false
         else window.open(url, '_blank');
     });
-
-
-
-//        $(document).on('change', "[id^='distribucion_nivel_']", function()
-//        {
-//            var valor=$(this).val();
-//            $('#'+$(this).attr('id') + ' option').removeAttr('selected');  //remueve si habia algun seleccionado
-//            $('#'+$(this).attr('id') + " option[value='"+valor+"']").attr('selected',true);
-//            var nivel=parseInt($(this).attr('name').substring(19));
-//
-//            if ($(this).val() != '')
-//            {
-//                jQuery.ajax({
-//                    success: function(html){
-//                        $('#distribucion_nivel_'+(nivel)).nextAll("[id^='distribucion_nivel_']").remove();
-//                        switch (nivel)
-//                        {
-//                            case 1:
-//                                $('#distribucion_nivel').empty().html(html);       //pare el inicial
-//                                break;
-//                            case 2:
-//                                $('#distribucion_nivel').append(html);
-//                                break;
-//                        }
-//                    },
-//                    fail: function(){
-//                        $('#notice').html('Hubo un error al cargar los filtros, por favor intentalo de nuevo.');
-//                    },
-//                    type:'POST',
-//                    url:'/regiones/regiones',
-//                    cache:true,
-//                    data: {region: $(this).val(), region_nivel: nivel}
-//                });
-//            }
-//
-//            if ($(this).val() == '' && nivel == 1)  //quita las sub-regiones si eligio todas
-//                $('#distribucion_nivel').empty();
-//
-//            if ($(this).val() == '' && nivel == 2)  //quita las sub-regiones si eligio todas
-//                $('#distribucion_nivel_3').remove();
-//        });
 });
 
