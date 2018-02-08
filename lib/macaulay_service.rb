edd7acb42b64bedd05ba281fdea2938aa67e4e16
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2018-02-07 22:52:12 -0600

    fix para mostrar texto en español enla respuesta de ebird

--- lib/macaulay_service.rb
+++ lib/macaulay_service.rb
@@ -3,7 +3,7 @@ class MacaulayService
   def dameMedia(taxonCode, type)
     ebird = CONFIG.ebird.api
 
-    url = "#{ebird}#{taxonCode}&mediaType=#{type}"
+    url = "#{ebird}#{taxonCode}&mediaType=#{type}&locale=es"
     url_escape = URI.escape(url)
     uri = URI.parse(url_escape)
     req = Net::HTTP::Get.new(uri.to_s)
@@ -18,40 +18,5 @@ class MacaulayService
     end
   end
 
-  def dameVideos(taxonCode)
-    ebird = CONFIG.ebird.api
-
-    url = "#{ebird}#{taxonCode}&mediaType=v&locale=es"
-    url_escape = URI.escape(url)
-    uri = URI.parse(url_escape)
-    req = Net::HTTP::Get.new(uri.to_s)
-    begin
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      res = http.request(req)
-      jres = JSON.parse(res.body)
-      jres['results']['content'] if jres.any?
-    rescue => e
-      nil
-    end
-  end
-
-  def dameAudios(taxonCode)
-    ebird = CONFIG.ebird.api
-
-    url = "#{ebird}#{taxonCode}&mediaType=a&locale=es"
-    url_escape = URI.escape(url)
-    uri = URI.parse(url_escape)
-    req = Net::HTTP::Get.new(uri.to_s)
-    begin
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      res = http.request(req)
-      jres = JSON.parse(res.body)
-      jres['results']['content'] if jres.any?
-    rescue => e
-      nil
-    end
-  end
 end
 
