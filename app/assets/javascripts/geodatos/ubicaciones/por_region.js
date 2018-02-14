3305a27f43535f26865be67b9ad37f1d900e6403
calonso-conabio <calonso@conabio.gob.mx>
2018-02-14 16:35:51 -0600

    los 3 layers incluido el de google y colores en los poligonos

--- app/assets/javascripts/geodatos/ubicaciones/por_region.js
+++ app/assets/javascripts/geodatos/ubicaciones/por_region.js
@@ -1,11 +1,34 @@
 var cargaMapa = function (id)
 {
+    // El default de leaflet
+    var OSM_layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',{
+        zIndex: 1,
+        noWrap: true
+    });
+
+    // Google terrain map layer
+    var GTM_layer = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
+        subdomains:['mt0','mt1','mt2','mt3'],
+        zIndex: 2,
+        noWrap: true
+    });
+    // Google Hybrid
+    var GHM_layer = L.tileLayer('http://{s}.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',{
+        subdomains:['mt0','mt1','mt2','mt3'],
+        zIndex: 3,
+        noWrap: true
+    });
+
     var place = [23.79162789, -102.04376221];
-    var tile_url = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
 
     map = L.map(id, {
         zoomControl: false,
-        doubleClickZoom: false
+        doubleClickZoom: false,
+        layers: [     // Existe un bug poniendo primero los layes de google
+            OSM_layer,
+            GTM_layer,
+            GHM_layer
+        ]
     });
 
     var zoom = L.control.zoom({
@@ -23,13 +46,17 @@ var cargaMapa = function (id)
     map.addControl(zoom);
     map.addControl(fullscreen);
 
-    var layer = L.tileLayer(tile_url, {
-        attribution: 'OSM',
-        noWrap: true
-    });
-
-    map.addLayer(layer);
     map.setView(place, 5);  // Default place and zoom
+
+    // Para poner las capas iniciales de los mapas
+    var baseMaps = {
+        "Open Street Maps": OSM_layer,
+        "Vista de terreno": GTM_layer,
+        "Vista Híbrida": GHM_layer
+    };
+
+    L.control.layers(baseMaps).addTo(map);
+    L.control.layers({}, {}, {collapsed: false, position: 'bottomleft'}).addTo(map);
 };
 
 /**
