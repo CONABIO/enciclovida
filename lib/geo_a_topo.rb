a0036657f5e197b8e33797126a88edb1be220407
calonso-conabio <calonso@conabio.gob.mx>
2018-02-15 20:28:56 -0600

    genera los topojson con comandos de linux

--- lib/geo_a_topo.rb
+++ lib/geo_a_topo.rb
@@ -8,4 +8,16 @@ class GeoATopo
 
     context.eval("topojson.topology({collection: #{collection} }, 1e4)")
   end
+
+  # Devuelve el topojson con un comando de linux
+  def dame_topojson_system(opc = {})
+    res = system("geo2topo -q #{opc[:q]} #{opc[:i]} > #{opc[:tmp]}")
+
+    if res
+      res = system("toposimplify -p #{opc[:p]} #{opc[:tmp]} > #{opc[:o]}")
+    end
+
+    File.delete(opc[:tmp])
+    res
+  end
 end
