6dfa5eef2089700e7c42ad1afd35095749f6a113
calonso-conabio <calonso@conabio.gob.mx>
2018-02-26 18:45:16 -0600

    ligero bug en el nombre de esta migracion

--- db/migrate/20161027180317_change_categoria_comentario_id_to_categoria_contenido_id.rb
+++ db/migrate/20161027180317_change_categoria_comentario_id_to_categoria_contenido_id.rb
@@ -1,4 +1,4 @@
-class ChangeCategoriaComentarioIdToCategoriasContenidoId < ActiveRecord::Migration
+class ChangeCategoriaComentarioIdToCategoriaContenidoId < ActiveRecord::Migration[5.1]
   def change
      change_table :comentarios do |t|
        t.rename :categoria_comentario_id, :categorias_contenido_id
