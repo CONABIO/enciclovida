7d78b9c4b3c32c10e3609e601e4ed83cd818b0be
calonso-conabio <calonso@conabio.gob.mx>
2018-02-26 20:04:12 -0600

    before_filter por before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :set_locale#, :authenticate  ##Autentica por credenciales generales
+  before_action :set_locale#, :authenticate  ##Autentica por credenciales generales
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def get_flickraw
