4d95ac0bdc85fd31c1f00258267785aa68e9e5bf
calonso-conabio <calonso@conabio.gob.mx>
2018-02-26 18:45:36 -0600

    ligero bug en el nombre de esta migracion

--- db/migrate/20161027174448_rename_categoria_comentario_to_categoria_contenido.rb
+++ db/migrate/20161027174448_rename_categoria_comentario_to_categoria_contenido.rb
@@ -1,4 +1,4 @@
-class RenameCategoriaComentarioToCategoriasContenido < ActiveRecord::Migration
+class RenameCategoriaComentarioToCategoriaContenido < ActiveRecord::Migration[5.1]
   def self.up
     rename_table "categorias_comentario", "categorias_contenido"
   end
