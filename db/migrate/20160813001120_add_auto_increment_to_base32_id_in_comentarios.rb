4eb250ca0787e23394d4ebc1eb17088958e95308
calonso-conabio <calonso@conabio.gob.mx>
2018-02-26 18:44:46 -0600

    simplificando la migraicon de base 32

--- db/migrate/20160813001120_add_auto_increment_to_base32_id_in_comentarios.rb
+++ db/migrate/20160813001120_add_auto_increment_to_base32_id_in_comentarios.rb
@@ -1,20 +1,15 @@
-class AddAutoIncrementToBase32IdInComentarios < ActiveRecord::Migration
+class AddAutoIncrementToBase32IdInComentarios < ActiveRecord::Migration[5.1]
   def up
-    add_column :comentarios, :idBak, :string
-    Comentario.update_all('idBak=id')
-    remove_column :comentarios, :id
-    add_index :comentarios, :created_at, unique: true
-    add_column :comentarios, :idConsecutivo, 'int NOT NULL IDENTITY (1,1) '
-    add_column :comentarios, :id, :nvarchar, limit: 10, null: false, default: ''
-    Comentario.update_all('id=dbo.ConvertToBase(idConsecutivo, 32)')  # Crear funcion o coorer script is_comentarios_a_base32
-    execute "ALTER TABLE comentarios ADD PRIMARY KEY (id);"
-
-    remove_index :comentarios, :created_at
+    change_table(:comentarios) do |t|
+      t.remove :id
+      t.integer :idConsecutivo
+      t.string :id, :limit => 10, :primary_key => true
+    end
   end
 
   def down
-    remove_column :comentarios, :idConsecutivo
-    Comentario.update_all('id=idBak')
-    remove_column :comentarios, :idBak
+    change_table(:comentarios) do |t|
+      t.remove :idConsecutivo
+    end
   end
 end
