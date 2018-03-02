b39555baec1819a9fe675444f1e97a3e6b8ff482
calonso-conabio <calonso@conabio.gob.mx>
2018-03-01 19:56:26 -0600

    EspecieRegion en la carpeta de catalogos

---
+++ app/models/catalogos/especie_region.rb
@@ -0,0 +1,18 @@
+class EspecieRegion < ActiveRecord::Base
+
+  establish_connection(:catalogos)
+  self.table_name = 'catalogocentralizado.RelNombreRegion'
+  self.primary_keys= :IdNombre, :IdRegion
+
+  # Los alias con las tablas de catalogos
+  alias_attribute :especie_id, :IdNombre
+  alias_attribute :region_id, :IdRegion
+
+  attr_accessor :region_id_falso
+  validates_presence_of :especie_id, :region_id
+  belongs_to :region
+  belongs_to :especie
+  belongs_to :tipo_distribucion, :foreign_key => TipoDistribucion.attribute_alias(:id)
+  has_many :nombres_regiones, :class_name => 'NombreRegion', :foreign_key => 'especie_id', :dependent => :destroy
+
+end
