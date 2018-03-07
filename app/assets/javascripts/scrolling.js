9c5af08c15f8ed12061fd36eb1bd291f28ee95de
calonso-conabio <calonso@conabio.gob.mx>
2018-03-06 19:43:02 -0600

    el scrolling en los diferentes tabs ya funciona en la busqueda avanzada

--- app/assets/javascripts/scrolling.js
+++ app/assets/javascripts/scrolling.js
@@ -1,7 +1,7 @@
 settings = {
-    nop     : 10, // The number of posts per scroll to be loaded
+    nop     : 100, // The number of posts per scroll to be loaded
     offset  : 2, // Initial offset, begins at 2 in this case
-    error   : 'No More Posts!', // When the user reaches the end this is the message that is
+    error   : '¡Has llegado al final de los resultados!', // When the user reaches the end this is the message that is
                                 // displayed. You can change this if you want.
     delay   : 500, // When you scroll down the posts will load after a delayed amount of time.
                    // This is mainly for usability concerns. You can alter this as you see fit
@@ -13,96 +13,85 @@ settings = {
 
 (function($) {
 
-	$.fn.scrollPagination = function(options) {
-		
-		// Extend the options so they work with the plugin
-		if(options) {
-			$.extend(settings, options);
-		}
-		
-		// For each so that we keep chainability.
-		return this.each(function() {
+    $.fn.scrollPagination = function(options) {
+
+        // Extend the options so they work with the plugin
+        if(options) {
+            $.extend(settings, options);
+        }
+
+        // For each so that we keep chainability.
+        return this.each(function() {
 
             if(settings.scroll == true) initmessage = 'Cargando... Por favor, espera <i class="spin3-ev-icon animate-spin" style="font-size: 3em; color: rgba(128, 0, 0, 0.75);"></i>';
-            else initmessage = 'Click for more';
+            else initmessage = 'Clic para cargar más';
 
-			function getData() {
+            function getData() {
+                $this = $("#resultados-" + settings.cat);
+                $('#loading-bar-' + settings.cat).remove();
+                $this.append("<div class='loading-bar col-xs-12 col-sm-12 col-md-12 col-lg-12' id='loading-bar-" + settings.cat + "'>" + initmessage+'</div>');
 
-                $this=$("#resultados" + settings.nivel);
-                $('#loading-bar' + settings.nivel).remove();
-                $this.append("<div class='loading-bar col-xs-12 col-sm-12 col-md-12 col-lg-12' id='loading-bar" + settings.nivel + "'>" +initmessage+'</div>');
+                $.get(settings.url, {
+                    pagina: settings.offset
+                }, function(data) {
+                    // Change loading bar content (it may have been altered)
+                    $('#loading-bar-' + settings.cat).html(initmessage);
 
-				$.get(settings.url, {
-						
-				    pagina         : settings.offset
-
-				}, function(data) {
-						
-					// Change loading bar content (it may have been altered)
-                    $('#loading-bar' + settings.nivel).html(initmessage);
-						
-					// If there is no data returned, there are no more posts to be shown. Show error
-					if(data == "") { 
-                        $('#loading-bar' + settings.nivel).html(settings.error);
+                    // If there is no data returned, there are no more posts to be shown. Show error
+                    if(data == "") {
+                        $('#loading-bar-' + settings.cat).html(settings.error);
                         settings.busy = false;  // Parche para que cuando acabe un scrolling de un TAB, siga cargando otros
-					}
-					else {
-						
-						// Offset increases
-					    settings.offset = settings.offset+1;
-                        eval("offset."+settings.cat + "=" + settings.offset);
+                    }
+                    else {
+                        // Offset increases
+                        settings.offset++;// = settings.offset + 1;
+                        offset[settings.cat] = settings.offset;
+                        //eval("offset."+settings.cat + "=" + settings.offset);
                         $this.append(data);
-                        $('#loading-bar' + settings.nivel).remove();
-						
-						// No longer busy!	
-						settings.busy = false;
-					}	
-						
-				});
-					
-			}	
-			
-            $this=$("#resultados" + settings.nivel);
+                        $('#loading-bar-' + settings.cat).remove();
 
-			// If scrolling is enabled
-			if(settings.scroll == true) {
-				// .. and the user is scrolling
-				$(window).scroll(function() {
-                    var $this=$("#resultados" + settings.nivel);
+                        // No longer busy!
+                        settings.busy = false;
+                    }
+                });
+            }
 
-					// Check the user is at the bottom of the element
+            $this = $("#resultados-" + settings.cat);
+
+            // If scrolling is enabled
+            if(settings.scroll == true) {
+                // .. and the user is scrolling
+                $(window).scroll(function() {
+                    var $this = $("#resultados-" + settings.cat);
+
+                    // Check the user is at the bottom of the element
                     if($(window).scrollTop() + $(window).height() + 400  > $(document).height() && !settings.busy) {
 
-						// Now we are working, so busy is true
-						settings.busy = true;
-						
-						// Tell the user we're loading posts
-                        $('#loading-bar' + settings.nivel).html('Cargando ...');
-						
-						// Run the function to fetch the data inside a delay
-						// This is useful if you have content in a footer you
-						// want the user to see.
-						setTimeout(function() {
+                        // Now we are working, so busy is true
+                        settings.busy = true;
 
+                        // Tell the user we're loading posts
+                        $('#loading-bar-' + settings.cat).html('Cargando... Por favor, espera <i class="spin3-ev-icon animate-spin" style="font-size: 3em; color: rgba(128, 0, 0, 0.75);"></i>');
+
+                        // Run the function to fetch the data inside a delay
+                        // This is useful if you have content in a footer you
+                        // want the user to see.
+                        setTimeout(function() {
                             getData();
+                        }, settings.delay);
 
-						}, settings.delay);
-							
-					}	
-				});
-			}
-			
-			// Also content can be loaded by clicking the loading bar/
-			$this.find("#resultados" + settings.nivel + '.loading-bar').click(function() {
-			
-				if(settings.busy == false) {
-					settings.busy = true;
-					getData();
-				}
-			
-			});
-			
-		});
-	}
+                    }
+                });
+            }
+
+            // Also content can be loaded by clicking the loading bar/
+            $this.find("#resultados-" + settings.cat + '.loading-bar').click(function() {
+                if(settings.busy == false) {
+                    settings.busy = true;
+                    getData();
+                }
+            });
+        });
+    }
 
 })(jQuery);
