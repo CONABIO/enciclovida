d28aaf58e7208bfaed4acfc8b2ae25cf5d35f8f7
calonso-conabio <calonso@conabio.gob.mx>
2018-04-06 19:25:03 -0500

    modelo bibliografia a carpeta de catalogos

--- app/models/catalogos/bibliografia.rb
+++ app/models/catalogos/bibliografia.rb
@@ -7,10 +7,6 @@ class Bibliografia < ActiveRecord::Base
   # Los alias con las tablas de catalogos
   alias_attribute :id, :IdBibliografia
 
-
-  has_many :nombres_regiones_bibligrafias
-  has_many :especies_bibliografias
-
   def personalizaBusqueda
     "#{self.autor} - #{self.titulo_publicacion} (#{self.anio})"
   end
