0d31be47a5ad213233277d186eb4e774b0cc6ca7
calonso-conabio <calonso@conabio.gob.mx>
2018-04-10 15:50:19 -0500

    un poco de limpieza

--- app/models/bibliografia_bio.rb
+++
@@ -1,8 +0,0 @@
-class BibliografiaBio < ActiveRecord::Base
-
-  self.table_name='Bibliografia'
-  self.primary_key='IdBibliografia'
-
-  has_many :nombres_regiones_bibligrafias
-  has_many :especies_bibliografias
-end
--- app/models/especie_bibliografia_bio.rb
+++
@@ -1,7 +0,0 @@
-class EspecieBibliografiaBio < ActiveRecord::Base
-  self.table_name='RelNombreBiblio'
-  self.primary_keys= :IdNombre, :IdBibliografia
-
-  belongs_to :especie
-  belongs_to :bibliografia
-end
