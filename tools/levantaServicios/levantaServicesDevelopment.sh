723bebd0677a67ed7524c3e0914b37545d38be78
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2018-04-24 19:17:14 -0500

    actualizacion en script q levanta en development

--- tools/levantaServicios/levantaServicesDevelopment.sh
+++ tools/levantaServicios/levantaServicesDevelopment.sh
@@ -2,6 +2,5 @@
 
 redis-server > log/bitacoraScript.log &
 sleep 10
-soulmate-web --foreground --no-launch --redis=redis://172.16.3.213:6379/0 >> log/bitacoraScript.log &
 rails runner "eval(File.read 'tools/levantaServicios/levantaBlurrily.rb')" &
 
