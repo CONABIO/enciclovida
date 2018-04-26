e31982b160356dc48b716da226719a22baeb95ca
calonso-conabio <calonso@conabio.gob.mx>
2018-04-25 19:46:15 -0500

    agrupando mejor el archivo de cache

--- lib/cache_services.rb
+++ lib/cache_services.rb
@@ -1,83 +1,86 @@
 module CacheServices
   # REVISADO: Actualiza todos los servicios concernientes a un taxon, se empaqueto para que no estuviera en Especie
   def servicios
-    suma_visita
+    guarda_redis_servicio
+    suma_visita_servicio
     cuantas_especies_inferiores(estadistica_id: 2)  # Servicio para poner el numero totales de especies del taxon
     cuantas_especies_inferiores(estadistica_id: 3)  # Servicio para poner el numero totales de especies o inferiores del taxon
     cuantas_especies_inferiores({estadistica_id: 22, validas: true})  # Servicio para poner el numero totales de especies o inferiores del taxon
     cuantas_especies_inferiores({estadistica_id: 23, validas: true})  # Servicio para poner el numero totales de especies o inferiores del taxon
-    guarda_observaciones_naturalista
-    guarda_ejemplares_snib
-  end
-
-  # REVISADO: Guarda las observaciones desde la pagina de naturalista
-  def guarda_observaciones_naturalista
-    if !@especie.existe_cache?('observaciones_naturalista')
-      @especie.delay(queue: 'observaciones_naturalista').guarda_observaciones_naturalista
-    end
-  end
-
-  # REVISADO: Guarda los ejemplares del SNIB
-  def guarda_ejemplares_snib
-    if !@especie.existe_cache?('ejemplares_snib')
-      @especie.delay(queue: 'ejemplares_snib').guarda_ejemplares_snib
-    end
+    guarda_observaciones_naturalista_servicio
+    guarda_ejemplares_snib_servicio
   end
 
   # REVISAO: Guarda los datos más importantes en el redis
-  def guarda_redis
-    @especie.delay(queue: 'redis').guarda_redis
+  def guarda_redis_servicio
+    if Rails.env.production?
+      delay(queue: 'redis').guarda_redis
+    else
+      guarda_redis
+    end
   end
 
   # REVISADO: Suma una visita a la estadisticas
-  def suma_visita
+  def suma_visita_servicio
     if Rails.env.production?
-      @especie.delay(queue: 'estadisticas').suma_visita
+      delay(queue: 'estadisticas').suma_visita
     else
-      @especie.suma_visita
+      suma_visita
     end
   end
 
   # REVISADO: Cuenta en numero de especies o el numero de especies mas las inferiores de una taxon, depende del argumento
-  def cuantas_especies_inferiores(opc = {})
-    if params[:action] == 'show'
+  def cuantas_especies_inferiores_servicio(opc = {})
+    if !@especie.existe_cache?("estadisticas_cuantas_especies_inferiores_#{opc[:estadistica_id]}")
       if Rails.env.production?
-        if !@especie.existe_cache?("estadisticas_cuantas_especies_inferiores_#{opc[:estadistica_id]}")
-          @especie.delay(queue: 'estadisticas').cuantas_especies_inferiores(opc)
-        end
+        @especie.delay(queue: 'estadisticas').cuantas_especies_inferiores(opc)
       else
         @especie.cuantas_especies_inferiores(opc)
       end
-    end  # if show
+    end
   end
 
-  def guarda_observaciones_naturalista
-    if p = proveedor
-      p.guarda_observaciones_naturalista
-    else
-      # Pone el cache para no volverlo a consultar, en caso que no tenga proveedor
+  # REVISADO: Guarda las observaciones desde la pagina de naturalista
+  def guarda_observaciones_naturalista_servicio
+    if !@especie.existe_cache?('observaciones_naturalista')
+      if p = proveedor
+        if Rails.env.production?
+          p.delay(queue: 'observaciones_naturalista').guarda_observaciones_naturalista
+        else
+          p.guarda_observaciones_naturalista
+        end
+      end
+
       escribe_cache('observaciones_naturalista', eval(CONFIG.cache.observaciones_naturalista)) if Rails.env.production?
     end
   end
 
-  def guarda_ejemplares_snib
-    if p = proveedor
-      p.guarda_ejemplares_snib
-    else
-      # Pone el cache para no volverlo a consultar, en caso que no tenga proveedor
+  # REVISADO: Guarda los ejemplares del SNIB
+  def guarda_ejemplares_snib_servicio
+    if !@especie.existe_cache?('observaciones_naturalista')
+      if p = proveedor
+        if Rails.env.production?
+          p.delay(queue: 'observaciones_naturalista').guarda_ejemplares_snib
+        else
+          p.guarda_ejemplares_snib
+        end
+      end
+
       escribe_cache('ejemplares_snib', eval(CONFIG.cache.ejemplares_snib)) if Rails.env.production?
     end
   end
 
-  # Los servicios no se actualizaran en menos de un dia
+  # REVISADO: Escribe un cache
   def escribe_cache(recurso, tiempo = 1.day)
     Rails.cache.write("#{recurso}_#{id}", true, :expires_in =>tiempo)
   end
 
+  # REVISADO: Verifica que el cache exista
   def existe_cache?(recurso)
     Rails.cache.exist?("#{recurso}_#{id}")
   end
 
+  # REVISADO: Borra un cache
   def borra_cache(recurso)
     Rails.cache.delete("#{recurso}_#{id}")
   end
