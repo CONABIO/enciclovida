d58c2d4709f79ef6d633c9c680c0cded665cff35
calonso-conabio <calonso@conabio.gob.mx>
2018-05-03 13:00:00 -0500

    estandarizando consulta de IUCN

--- lib/iucn_service.rb
+++ lib/iucn_service.rb
@@ -4,14 +4,14 @@ class IUCNService
     @iucn = CONFIG.iucn.api
     @token = CONFIG.iucn.token
 
-    url = "#{@iucn}/api/v3/species/#{opts[:nombre]}?token=#{@token}"
+    url = "#{@iucn}/api/v3/species/#{opts[:nombre].limpia_ws}?token=#{@token}"
     url_escape = URI.escape(url)
     uri = URI.parse(url_escape)
     req = Net::HTTP::Get.new(uri.to_s)
     begin
       res = Net::HTTP.start(uri.host, uri.port) {|http| http.request(req) }
       jres = JSON.parse(res.body)['result']
-      jres[0]["category"] if jres.any?
+      jres[0]['category'] if jres.any?
     rescue => e
       nil
     end
