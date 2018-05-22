887782c3fae6ebdb82614372d64797d7ab6ff169
calonso-conabio <calonso@conabio.gob.mx>
2018-05-22 12:19:13 -0500

    cache para el catalogo de criterios y actualiacion de base

--- app/models/peces/criterio.rb
+++ app/models/peces/criterio.rb
@@ -9,18 +9,35 @@ class Criterio < ActiveRecord::Base
   belongs_to :propiedad
 
   def self.catalogo
-    grouped_options = {}
 
-    Criterio.select(:id, :propiedad_id).group(:propiedad_id).each do |c|
-      prop = c.propiedad
-      llave_unica = prop.ancestors.map(&:nombre_propiedad).join('/')
+    resp = Rails.cache.fetch('criterios_catalogo') do
+      grouped_options = {}
 
-      grouped_options[llave_unica] = [] if !grouped_options.key?(llave_unica)
-      grouped_options[llave_unica] << [prop.nombre_propiedad, c.id]
+      Criterio.select(:id, :propiedad_id).group(:propiedad_id).each do |c|
+        prop = c.propiedad
+        llave_unica = prop.ancestors.map(&:nombre_propiedad).join('/')
+
+        grouped_options[llave_unica] = [] if !grouped_options.key?(llave_unica)
+        grouped_options[llave_unica] << [prop.nombre_propiedad, c.id]
+      end
+
+      grouped_options
     end
 
-    grouped_options
+    resp
+  end
 
+  # Cache de algunas cosas
+  def escribe_cache(recurso, tiempo = 1.day)
+    Rails.cache.write(recurso, :expires_in =>tiempo)
+  end
+
+  def existe_cache?(recurso)
+    Rails.cache.exist?(recurso)
+  end
+
+  def borra_cache(recurso)
+    Rails.cache.delete(recurso)
   end
 
 end
