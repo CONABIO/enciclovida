fb8e55051c3462cf770b8b1b34cab9bd9fb49df5
calonso-conabio <calonso@conabio.gob.mx>
2018-05-22 20:44:55 -0500

    los filtros de la busqueda se generan en criterios para seleccionar el ID

--- app/models/peces/criterio.rb
+++ app/models/peces/criterio.rb
@@ -8,9 +8,20 @@ class Criterio < ActiveRecord::Base
 
   belongs_to :propiedad
 
+  scope :join_propiedades,  -> { joins('LEFT JOIN propiedades ON propiedades.id=criterios.propiedad_id') }
+  #scope :grupos_conabio, -> { where(nombre_propiedad: 'Grupo CONABIO').first.children.order(:nombre_propiedad) }
+  #scope :zonas, -> { where(nombre_propiedad: 'Zonas').first.children }
+  scope :tipo_capturas, -> { select('criterios.id, nombre_propiedad').join_propiedades.where("ancestry=?", 320) }
+  scope :tipo_vedas, -> { where(nombre_propiedad: 'Tipo de veda').first.children }
+  scope :procedencias, -> { where(nombre_propiedad: 'Procedencia').first.children }
+  scope :pesquerias, -> { select(:nombre_propiedad).where(tipo_propiedad: 'Pesquerías en vías de sustentabilidad ').distinct.order(:nombre_propiedad) }
+  scope :cnp, -> { select(:nombre_propiedad).where(tipo_propiedad: ['zona pacifico', 'zona golfo y caribe']).distinct.order(:nombre_propiedad) }
+  scope :nom, -> { where(nombre_propiedad: 'Norma Oficial Mexicana 059 SEMARNAT-2010').first.children.order(:nombre_propiedad) }
+  scope :iucn, -> { where(nombre_propiedad: 'Lista roja IUCN 2016-3').first.children.order(:nombre_propiedad) }
+
   def self.catalogo
 
-    resp = Rails.cache.fetch('criterios_catalogo') do
+    resp = Rails.cache.fetch('criterios_catalogo', expires_in: eval(CONFIG.cache.peces.catalogos)) do
       grouped_options = {}
 
       Criterio.select(:id, :propiedad_id).group(:propiedad_id).each do |c|
@@ -25,19 +36,6 @@ class Criterio < ActiveRecord::Base
     end
 
     resp
-  end
-
-  # Cache de algunas cosas
-  def escribe_cache(recurso, tiempo = 1.day)
-    Rails.cache.write(recurso, :expires_in =>tiempo)
-  end
-
-  def existe_cache?(recurso)
-    Rails.cache.exist?(recurso)
-  end
-
-  def borra_cache(recurso)
-    Rails.cache.delete(recurso)
   end
 
 end
