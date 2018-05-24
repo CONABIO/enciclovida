0ce3042b90d529d7fa04a031ec4bd727356a07db
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2018-05-23 23:27:15 -0500

    fixes en los estilos y el js para hacerlo más coqueto

--- app/assets/javascripts/peces/peces_extra.js
+++ app/assets/javascripts/peces/peces_extra.js
@@ -18,7 +18,8 @@ $( function() {
             $( "#especie_id" ).val( ui.item.especie_id );
             $( "#ncientifico" ).val( ui.item.value );
             $( "#ncomunes" ).val('');
-            $( ".agrupada select, #ncomunes" ).attr('disabled', true).addClass('disabled');
+            $( ".valorada select, .valorada input, .valorada span, .agrupada select, .calificada select, .calificada input, .calificada span, #ncomunes" ).attr('disabled', true).addClass('disabled');
+            $( "#rango" ).slider( "disable" );
             return false;
         }
     });
@@ -44,7 +45,8 @@ $( function() {
             $( "#especie_id" ).val( ui.item.especie_id );
             $( "#ncomunes" ).val( ui.item.value );
             $( "#ncientifico" ).val('');
-            $( ".agrupada select, #ncientifico" ).attr('disabled', true).addClass('disabled');
+            $( ".valorada select, .valorada input, .valorada span, .agrupada select, .calificada select, .calificada input, .calificada span, #ncientifico" ).attr('disabled', true).addClass('disabled');
+            $( "#rango" ).slider( "disable" );
             return false;
         }
     });
@@ -56,7 +58,7 @@ $( function() {
         max: 70,
         values: [-5,4],
         slide: function( event, ui ) {
-            $( "#valor_total" ).val(ui.values[ 0 ] + "," + ui.values[ 1 ] );
+            $( "#valor_total" ).val(ui.values[ 0 ] + ", " + ui.values[ 1 ] );
             $("#resultados").load('/peces/busqueda?semaforo_vt=' + $( "#rango" ).slider( "values", 0 ) + ',' + $( "#rango" ).slider( "values", 1 ));
         }
     });
@@ -64,6 +66,7 @@ $( function() {
   } );
 
 function limpiaBusqueda(){
-    $(".agrupada select, #ncientifico, #ncomunes").attr("disabled", false).removeClass("disabled");
-    $( "#especie_id, #ncientifico, #ncomunes" ).val('');
+    $(".valorada *, .agrupada *, .calificada *, #ncientifico, #ncomunes").attr("disabled", false).removeClass("disabled");
+    $( "#rango" ).slider( "enable" );
+    $( "#especie_id, #ncientifico, #ncomunes, #valor_total" ).val('');
 };
