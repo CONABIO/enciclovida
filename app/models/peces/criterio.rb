ead52e47f7b7cceb8071736f8930bfc2f269461e
calonso-conabio <calonso@conabio.gob.mx>
2018-06-12 16:17:51 -0500

    algunos cambios como el campo con_estrella

--- app/models/peces/criterio.rb
+++ app/models/peces/criterio.rb
@@ -24,7 +24,7 @@ class Criterio < ActiveRecord::Base
   def self.catalogo(prop = nil)
 
     if prop.present?
-      prop.siblings.map { |p| [p.nombre_propiedad, p.criterios.first.id] }
+      prop.siblings.map { |p| [p.nombre_propiedad, p.criterios.first.id] if p.criterios.present? }
 
     else
       resp = Rails.cache.fetch('criterios_catalogo', expires_in: eval(CONFIG.cache.peces.catalogos)) do
