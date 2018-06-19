d46e71e1c06b1392b91c68ebb21bf525df4a4c62
calonso-conabio <calonso@conabio.gob.mx>
2018-06-19 13:46:48 -0500

    arreglando los js de peces

--- app/assets/javascripts/peces/peces.js
+++ app/assets/javascripts/peces/peces.js
@@ -1,25 +1,11 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
-//= require jquery-ui.js
-// require jquery_ujs
+//= require jquery-ui
 //= require turbolinks
-//= require adicional.js
 //= require bootstrap-sprockets
-//= require i18n.min.js
-//= require general.js
-//= require jquery.soulmate.js
-//= require busquedas.js
-//= require "i18n/translations"
-//= require "peces/peces_extra"
+//= require i18n.min
+//= require general
+//= require jquery.soulmate
+//= require busquedas
+//= require i18n/translations
+//= require peces/peces_extra
 //= require cocoon
