d8b176abe24c2db7be933fc33e42c55a7a0e1476
calonso-conabio <calonso@conabio.gob.mx>
2018-06-20 19:00:27 -0500

    limpiando el layout principal

--- app/assets/javascripts/especies/mapa/mapa_extra.js
+++ app/assets/javascripts/especies/mapa/mapa_extra.js
@@ -3,3 +3,8 @@ var dameUrlServicioSnibFicha = function(prop)
     var snib_url = prop.snib_url + '/snib/getSpecies/' + prop.reino + '/' + prop.catalogo_id + '/?apiKey=enciclovida';
     return snib_url;
 };
+
+var dameCargaRegistrosSnib = function (prop) {
+    var url = dameUrlServicioSnibFicha(prop);
+    cargaRegistrosSnib(url);
+};
