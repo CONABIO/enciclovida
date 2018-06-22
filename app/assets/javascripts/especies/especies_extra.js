60fedc6f0d53c342d9bc71852f463db73bf3fab0
calonso-conabio <calonso@conabio.gob.mx>
2018-06-21 19:02:48 -0500

    limpiando los assets en la ficha de la especie , parte V

--- app/assets/javascripts/especies/especies_extra.js
+++ app/assets/javascripts/especies/especies_extra.js
@@ -1,63 +1,27 @@
-/**
- * // Inicializa las funciones del show de especies
- */
-var showEspecies  =function()
-{
+$(document).ready(function(){
     tooltip();
     refreshMediaQueries();
-    nombresComunes();
-    eventoPestañas();
 
-    // Para correr las imagenes principales
-    if (INATURALIST_API != undefined) fotos_naturalista(); else fotos_bdi();
-};
-
-// Pone el evento en las pestañas
-var eventoPestañas = function()
-{
     $('#pestañas > .nav a').one('click',function(){
         if (!Boolean($(this).hasClass('noLoad'))){
             var idPestaña = $(this).data('params') || this.getAttribute('href').replace('#','');
-            var pestaña = '/especies/'+TAXON.id+'/'+idPestaña;
+            var pestaña = '/especies/' + opciones.taxon + '/'+idPestaña;
             $(this.getAttribute('href')).load(pestaña);
         }
     });
-};
 
-// Para correr los nobres comunes del lado del cliente, pone de catalogos y naturalista
-var nombresComunes = function()
-{
-    $('#nombres_comunes_todos').load("/especies/" + TAXON.id + "/nombres-comunes-todos");
-};
+    if (opciones.naturalista_api != undefined) fotos_naturalista(); else fotos_bdi();
 
-$(document).ready(function(){
+    $('#nombres_comunes_todos').load("/especies/" + opciones.taxon + "/nombres-comunes-todos");
 
     $('#enlaces_externos').on('click', '#boton_pdf', function(){
-        window.open("/especies/"+TAXON.id+".pdf?from="+cual_ficha);
+        window.open("/especies/" + opciones.taxon + ".pdf?from=" + opciones.cual_ficha);
     });
-
-    var info = function(){
-        $('#ficha-div').slideUp();
-        $('#info-div').slideDown();
-        return false;
-    };
-
-    var ficha = function(){
-        $('#info-div').slideUp();
-        $('#ficha-div').slideDown();
-        return false;
-    };
-
-    var muestraBibliografiaNombres = function(ident){
-        var id=ident.split('_')[2];
-        $("#biblio_"+id).dialog();
-        return false;
-    };
 
     $(document).on('click', '.historial_ficha', function(){
         var comentario_id = $(this).attr('comentario_id');
         var especie_id = $(this).attr('especie_id');
-        $("#historial_ficha_" + comentario_id).load("/especies/"+especie_id+"/comentarios/"+comentario_id+"/respuesta_externa?ficha=1");
+        $("#historial_ficha_" + comentario_id).load("/especies/" + especie_id + "/comentarios/" + comentario_id + "/respuesta_externa?ficha=1");
         $("#historial_ficha_" + comentario_id).slideDown();
         return false;
     });
--- app/assets/javascripts/especies/fichas/fichas_extra.js
+++ app/assets/javascripts/especies/fichas/fichas_extra.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-    cual_ficha = '';
-
     $('#taxon_description').on('change', '#from', function () {
-        cual_ficha = $(this).val();
+        opciones.cual_ficha = $(this).val();
 
         $.ajax({
-            url: "/especies/" + TAXON.id + "/describe?from=" + cual_ficha,
+            url: "/especies/" + opciones.taxon + "/describe?from=" + opciones.cual_ficha,
             method: 'get',
             success: function (data, status) {
                 $('.taxon_description').replaceWith(data);
