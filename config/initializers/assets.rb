3ce3646484386edbb83ff2b7be153194d572aed7
calonso-conabio <calonso@conabio.gob.mx>
2018-06-22 20:56:23 -0500

    acomodando assets de bdi

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,5 @@
 Rails.application.config.assets.precompile += %w(
-especies/especies.js especies/fichas/fichas_extra.js
+especies/especies.js especies/fichas/fichas_extra.js especies/media/bdi.js
 inicio.css busquedas.css scrolling.js carrusel.css especies/obligatorios.js
 especies/arbol/arbol_identado.scss especies/arbol/arbol_nodo.css especies/arbol/arbol_nodo.js especies/arbol/arbol_identado.js
 especies/geodatos.js
