2dc8b3f0c18794ff815b3d354e238b78f6944c3e
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2018-07-07 02:07:39 -0500

    compactacion de la funcion de checboxes en el helper, ya solo falta fusionasr con las zonas

--- app/helpers/peces_helper.rb
+++ app/helpers/peces_helper.rb
@@ -3,7 +3,6 @@ module PecesHelper
 
     nombre = taxon.nombres_comunes.present? ? taxon.nombres_comunes.split(',').first : ''
 
-
     if params[:title]
       nombre.present? ? "<h4>#{nombre}</h4> <h5>#{taxon.nombre_cientifico}</h5>".html_safe : taxon.nombre_cientifico
     elsif params[:link]
@@ -17,27 +16,32 @@ module PecesHelper
 
   def checkboxRecomendacion
     checkBoxes = ''
-    s = {'Recomendable' => 'v','Poco recomendable' => 'a','Evita' => 'r', 'Sin datos' => 'sn'}
+    s = {:v => ['Recomendable','glyphicon glyphicon-ok-sign'], :a => ['Poco recomendable','glyphicon glyphicon-exclamation-sign'], :r => ['Evita','glyphicon glyphicon-minus-sign'], :sn => ['Sin datos','no-data-ev-icon']}
     s.each do |k,v|
       checkBoxes << "<label>"
-      checkBoxes << check_box_tag('semaforo_recomendacion[]', v, false, id: "semaforo_recomendacion_#{k}")
-      checkBoxes << "<span title = '#{k}' class = 'btn btn-lg btn-basica btn-zona-#{v} btn-title'>"
-      checkBoxes << "<i class = '"
-      checkBoxes << case k
-                    when 'Recomendable'
-                      'glyphicon glyphicon-ok-sign'
-                    when 'Poco recomendable'
-                      'glyphicon glyphicon-exclamation-sign'
-                    when 'Evita'
-                      'glyphicon glyphicon-minus-sign'
-                    when 'Sin datos'
-                      'no-data-ev-icon'
-                    else
-                      'stop'
-                    end
-      checkBoxes << "'></i>"
+      checkBoxes << check_box_tag('semaforo_recomendacion[]', k, false, id: "semaforo_recomendacion_#{v[0]}")
+      checkBoxes << "<span title = '#{v[0]}' class = 'btn btn-lg btn-basica btn-zona-#{k} btn-title'>"
+      checkBoxes << "<i class = '#{v[1]}'></i>"
       checkBoxes << "</span>"
       checkBoxes << "</label>"
+    end
+    checkBoxes.html_safe
+  end
+
+
+  def checkboxCriteriosPeces(cat, ico=false, titulo='')
+    checkBoxes="<h5><strong>#{titulo}</strong></h5>"
+
+    cat.each do |k, valores|
+      valores.each do |edo, id|
+        checkBoxes << "<label>"
+        checkBoxes << check_box_tag("#{k}[]", id, false, :id => "#{k}_#{edo.parameterize}")
+        checkBoxes << "<span title = '#{edo}' class = '#{k} btn btn-xs btn-basica btn-title #{'btn-default' unless ico}'>"
+        checkBoxes << "#{edo}" unless ico
+        checkBoxes << "<i class = '#{edo.parameterize}-ev-icon'></i>" if ico
+        checkBoxes << "</span>"
+        checkBoxes << "</label>"
+      end
     end
     checkBoxes.html_safe
   end
@@ -48,41 +52,7 @@ module PecesHelper
     @filtros[:zonas].each_with_index do |z, i|
       lista << "<li tooltip-title='#{z[0]}' class='btn-title btn-zona btn-zona-#{pez.valor_zonas[i]}'>#{z[0].split(' ').last}</li>"
     end
-      lista << '</ul>'
+    lista << '</ul>'
   end
 
-  # Filtros para Categorías de riesgo y comercio internacional
-  def checkboxEstadoConservacionPeces(cat)
-    checkBoxes=''
-
-    cat.each do |k, valores|
-      checkBoxes << "<h5><strong>#{t(k)}</strong></h5>"
-      valores.each do |edo, id|
-        checkBoxes << "<label>"
-        checkBoxes << check_box_tag("#{k}[]", id, false, :id => "#{k}_#{edo.parameterize}")
-        checkBoxes << "<span title = '#{t('cat_riesgo.' << edo.parameterize << '.nombre')}' class = 'btn btn-xs btn-basica btn-title'>"
-        checkBoxes << "<i class = '#{edo.parameterize}-ev-icon'></i>"
-        checkBoxes << "</span>"
-        checkBoxes << "</label>"
-      end
-    end
-    checkBoxes.html_safe
-  end
-
-  # Filtros para Categorías de riesgo y comercio internacional
-  def checkboxPeces(cat)
-    checkBoxes=''
-
-    cat.each do |k, valores|
-      valores.each do |edo, id|
-        checkBoxes << "<label>"
-        checkBoxes << check_box_tag("#{k}[]", id, false, :id => "#{k}_#{edo.parameterize}")
-        checkBoxes << "<span title = '#{edo}' class = 'btn btn-xs btn-default btn-basica btn-title #{k}'>"
-        checkBoxes << "#{edo}"
-        checkBoxes << "</span>"
-        checkBoxes << "</label>"
-      end
-    end
-    checkBoxes.html_safe
-  end
 end
