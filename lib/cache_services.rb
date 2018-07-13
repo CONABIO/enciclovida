aff860cbbb67c586327c07f36d3d31f2ad9cfed6
calonso-conabio <calonso@conabio.gob.mx>
2018-07-13 15:58:46 -0500

    agrupando algunas funciones del modelo especies en cache

--- lib/cache_services.rb
+++ lib/cache_services.rb
@@ -106,4 +106,183 @@ module CacheServices
   def borra_cache(recurso)
     Rails.cache.delete("#{recurso}_#{id}")
   end
+
+
+  private
+
+  # REVISADO: Asigna el redis correspondiente
+  def asigna_redis(opc={})
+    datos = {}
+    datos[:data] = {}
+
+    guarda_fotos_nombres_servicios if opc[:consumir_servicios]
+    visitas = especie_estadisticas.visitas
+
+    # Asigna si viene la peticion de nombre comun
+    if nc = opc[:nombre_comun]
+      datos[:id] = nc.id
+      datos[:term] = I18n.transliterate(nc.nombre_comun.limpia)
+      datos[:data][:nombre_comun] = nc.nombre_comun
+      datos[:data][:id] = id
+      datos[:data][:lengua] = nc.lengua
+
+      # Para el score dependiendo la lengua
+      lengua = nc.lengua.estandariza
+      index = Adicional:: LENGUAS_ACEPTADAS.reverse.index(lengua) || 0
+      datos[:score] = index*visitas
+
+    else  # Asigna si viene la peticion de nombre_cientifico
+      datos[:id] = id
+      datos[:term] = I18n.transliterate(nombre_cientifico.limpia)
+      datos[:data][:nombre_comun] = x_nombre_comun_principal
+      datos[:data][:id] = id
+      datos[:data][:lengua] = x_lengua
+      datos[:score] = Adicional::LENGUAS_ACEPTADAS.length*visitas
+    end
+
+    if opc[:foto_principal].present?
+      datos[:data][:foto] = opc[:foto_principal]
+    else
+      datos[:data][:foto] = x_square_url  # Foto square_url
+    end
+
+    datos[:data][:nombre_cientifico] = nombre_cientifico.limpia
+    datos[:data][:estatus] = Especie::ESTATUS_VALOR[estatus]
+    datos[:data][:autoridad] = nombre_autoridad.try(:limpia)
+
+    # Caracteristicas de riesgo y conservacion, ambiente y distribucion
+    cons_amb_dist = {}
+    caracteristicas = nom_cites_iucn_ambiente_prioritaria(iucn_ws: true) << tipo_distribucion
+
+    caracteristicas.reduce({}, :merge).each do |nombre, valores|
+      next unless valores.any?
+
+      valores.each do |valor|
+        cons_amb_dist[valor.estandariza] = valor
+      end
+    end
+
+    datos[:data][:cons_amb_dist] = cons_amb_dist
+
+    # Para saber cuantas fotos tiene
+    datos[:data][:fotos] = x_fotos_totales
+
+    # Para saber si tiene algun mapa
+    if p = proveedor
+      datos[:data][:geodatos] = p.geodatos[:cuales]
+    end
+
+    datos.stringify_keys
+  end
+
+  # REVISADO: Gurada los nombres comunes y cientifico en redis
+  def guarda_redis(opc={})
+    # Pone en nil las variables para guardar los servicios y no consultarlos de nuevo
+    self.x_foto_principal = nil
+    self.x_nombre_comun_principal = nil
+    self.x_lengua = nil
+    self.x_fotos_totales = 0  # Para poner cero si no tiene fotos
+    self.x_nombres_comunes = nil
+    self.x_nombres_comunes_todos = nil
+
+    if opc[:loader].present? # Guarda en el loader que especifico
+      loader = Soulmate::Loader.new(opc[:loader])
+    else # Guarda en la cataegoria taxonomica correspondiente
+      categoria = I18n.transliterate(categoria_taxonomica.nombre_categoria_taxonomica).gsub(' ','_')
+      loader = Soulmate::Loader.new(categoria)
+    end
+
+    # Borra los actuales
+    borra_redis(loader)
+
+    # Guarda el redis con el nombre cientifico
+    loader.add(asigna_redis(opc.merge({consumir_servicios: true})))
+
+    # Guarda el redis con todos los nombres comunes
+    num_nombres = 0;
+
+    x_nombres_comunes_todos.each do |nombres|
+      lengua = nombres.keys.first
+
+      nombres.values.flatten.each_with_index do |nombre|
+        num_nombres+= 1
+        nombre_obj = NombreComun.new({id: "#{id}000000#{num_nombres}".to_i, nombre_comun: nombre, lengua: lengua})
+        loader.add(asigna_redis(opc.merge({nombre_comun: nombre_obj})))
+      end
+    end
+  end
+
+  # REVISADO: borra todos los nombres comunes y el cnetifico del redis, para posteriormente volver a generarlo
+  def borra_redis(loader)
+    # Borra el del nombre cientifico
+    nombre_cient_data = {id: id}.stringify_keys
+    loader.remove(nombre_cient_data)
+
+    # Borra los nombre comunes
+    50.times do |i|
+      nombre_com_data = {id: "#{id}000000#{i+1}"}.stringify_keys
+      loader.remove(nombre_com_data)
+    end
+  end
+
+  # REVISADO: Regresa el numero de especies
+  def cuantas_especies(opc = {})
+    scope = descendants.solo_especies
+    scope = scope.where(estatus: 2) if opc[:validas]
+    scope.count
+  end
+
+  # REVISADO: Regresa el numero de especies e inferiores
+  def cuantas_especies_e_inferiores(opc = {})
+    scope = descendants.especies_e_inferiores
+    scope = scope.where(estatus: 2) if opc[:validas]
+    scope.count
+  end
+
+  # REVISADO: Pone el conteo de las especies o inferiores de un taxon en la tabla estadisticas
+  def cuantas_especies_inferiores(opc = {})
+    return unless opc[:estadistica_id].present?
+    puts "\n\nGuardo estadisticas_cuantas_especies_inferiores_#{opc[:estadistica_id]} - #{id} ..."
+    escribe_cache("estadisticas_cuantas_especies_inferiores_#{opc[:estadistica_id]}", eval(CONFIG.cache.estadisticas.cuantas_especies_inferiores)) if Rails.env.production?
+
+    conteo = case opc[:estadistica_id]
+             when 2, 22
+               cuantas_especies(opc)
+             when 3, 23
+               cuantas_especies_e_inferiores(opc)
+             else
+               false
+             end
+
+    return unless conteo
+
+    if estadistica = especie_estadisticas.where(estadistica_id: opc[:estadistica_id]).first
+      estadistica.conteo = conteo
+      estadistica.save if estadistica.changed?
+      return
+    end
+
+    # Quiere decir que no existia la estadistica
+    estadistica = especie_estadisticas.new
+    estadistica.estadistica_id = opc[:estadistica_id]
+    estadistica.conteo = conteo
+    estadistica.save
+  end
+
+  # REVISADO: Suma la visita de una ficha en la tabla estadisticas
+  def suma_visita
+    puts "\n\nGuardo conteo de visitas #{id} ..."
+
+    if estadistica = especie_estadisticas.where(estadistica_id: 1).first
+      estadistica.conteo+= 1
+      estadistica.save
+      return
+    end
+
+    # Quiere decir que no existia la estadistica
+    estadistica = especie_estadisticas.new
+    estadistica.estadistica_id = 1
+    estadistica.conteo = 1
+    estadistica.save
+  end
 end
