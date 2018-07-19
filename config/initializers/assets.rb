84832027e8ed54e512f6ed65bb05219a94c96f22
calonso-conabio <calonso@conabio.gob.mx>
2018-07-18 19:17:41 -0500

    quitando asset inecesario en bsuqueda por region

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,8 +4,7 @@ busquedas/avanzada/avanzada.js lib/scrolling.js
 especies/especies.js especies/fichas/fichas_extra.js especies/media/bdi.js especies/media/multimedia.js
 inicio.css busquedas.css carrusel.css especies/obligatorios.js
 especies/arbol/arbol_identado.scss especies/arbol/arbol_nodo.css especies/arbol/arbol_nodo.js especies/arbol/arbol_identado.js
-especies/geodatos.js
-busquedas/region/por_region.js busquedas/region/por_region.css busquedas/region/por_region_delayed.js
+busquedas/region/por_region.js busquedas/region/por_region.css busquedas/region/por_region_extra.js
 peces/peces.js peces/peces.css
 especies/mapa/mapa.js especies/mapa/mapa.css especies/mapa/mapa_delayed.js
 )
