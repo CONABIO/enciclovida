de724a720955553c60efc1e15982654b4f199065
calonso-conabio <calonso@conabio.gob.mx>
2018-07-25 18:07:03 -0500

    comentarios funcionando en centralizacion

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,10 +1,11 @@
 Rails.application.config.assets.precompile += %w(
 inicio/inicio.js soulmate.css
-busquedas/avanzada/avanzada.js lib/scrolling.js
+busquedas/avanzada/avanzada.js
 especies/especies.js especies/fichas/fichas_extra.js especies/media/bdi.js especies/media/multimedia.js
 inicio.css busquedas.css carrusel.css especies/obligatorios.js
 especies/arbol/arbol_identado.scss especies/arbol/arbol_nodo.css especies/arbol/arbol_nodo.js especies/arbol/arbol_identado.js
 busquedas/region/por_region.js busquedas/region/por_region.css busquedas/region/por_region_extra.js
 peces/peces.js peces/peces.css
 especies/mapa/mapa.js especies/mapa/mapa.css especies/mapa/mapa_delayed.js
+comentarios/comentarios.js
 )
