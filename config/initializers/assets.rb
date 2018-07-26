7e55ad45b71bf570b8dbc219741af71f90385f4e
calonso-conabio <calonso@conabio.gob.mx>
2018-07-26 13:20:09 -0500

    el viejo idMillon redirecciona al nuevo ID centralizacion

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,6 +1,6 @@
 Rails.application.config.assets.precompile += %w(
 inicio/inicio.js soulmate.css
-busquedas/avanzada/avanzada.js
+busquedas/avanzada/avanzada.js lib/scrolling.js
 especies/especies.js especies/fichas/fichas_extra.js especies/media/bdi.js especies/media/multimedia.js
 inicio.css busquedas.css carrusel.css especies/obligatorios.js
 especies/arbol/arbol_identado.scss especies/arbol/arbol_nodo.css especies/arbol/arbol_nodo.js especies/arbol/arbol_identado.js
