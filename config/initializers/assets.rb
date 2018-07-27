c04f0d1238b3c49058e1f24a562240a5eb652241
calonso-conabio <calonso@conabio.gob.mx>
2018-07-26 19:14:26 -0500

    el script de ocmentarios en la ficha se soluciono para no repetir codigo

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 Rails.application.config.assets.precompile += %w(
 inicio/inicio.js soulmate.css
 busquedas/avanzada/avanzada.js lib/scrolling.js
-especies/especies.js especies/fichas/fichas_extra.js especies/media/bdi.js especies/media/multimedia.js
+especies/especies.js especies/fichas/fichas_extra.js especies/media/bdi.js especies/media/multimedia.js especies/comentarios/comentarios_extra.js
 inicio.css busquedas.css carrusel.css especies/obligatorios.js
 especies/arbol/arbol_identado.scss especies/arbol/arbol_nodo.css especies/arbol/arbol_nodo.js especies/arbol/arbol_identado.js
 busquedas/region/por_region.js busquedas/region/por_region.css busquedas/region/por_region_extra.js
