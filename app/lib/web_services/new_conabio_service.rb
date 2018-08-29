c0e696972a6ac044a2029b1b2e735b79e886b31d
MartiHS <2123064604@alumnos.cua.uam.mx>
2018-08-28 21:52:54 -0500

    Corrección de Puerto harcodeado

--- app/lib/web_services/new_conabio_service.rb
+++ app/lib/web_services/new_conabio_service.rb
@@ -6,7 +6,7 @@ class New_Conabio_Service
 
   def initialize(options = {})
     @service_name = 'CONABIO_FICHAS'
-    @server = options[:server] || 'localhost:3000'
+    @server = options[:server] || "#{IP}:#{PORT}" # después del puerto, termina con '/'
     @timeout = options[:timeout] || 8
     @debug = options[:debug] || false
     Rails.logger.debug "[DEBUG] Inicializar el servicio: #{@service_name}"
@@ -15,10 +15,6 @@ class New_Conabio_Service
   def search(q)
     Rails.logger.debug "[DEBUG] Se realizará la busqueda de: #{q}"
     # Llamar a 'infoEspecie', quien nos devolverá código html con la infomración de la especie
-
-    #@resultado = "o&Atilde;&sup3;n"
-    #@resultado.encode('iso-8859-1')
-
     # Por prueba, se envia el taxón 1, pero q, se envía el que se va a buscar realmente
     request('infoEspecie/1', q)
   end
@@ -29,11 +25,9 @@ class New_Conabio_Service
     begin
       timed_out = Timeout::timeout(@timeout) do
         Rails.logger.debug "[DEBUG] #{self.class.name} getting #{request_uri}"
+        #Nokogiri::HTML(open(request_uri), nil, 'ISO-8859-1')
         Nokogiri::HTML(open(request_uri))
-        #@timeout.encode('iso-8859-1').force_encoding('UTF-8')
       end
-      #timed_out.encoding
-      #Rails.logger.debug "[DEBUG] Resultado: #{timed_out}"
     rescue Timeout::Error
       raise Timeout::Error, "#{@service_name} didn't respond within #{@timeout} seconds."
     end
@@ -41,7 +35,7 @@ class New_Conabio_Service
 
   def get_uri(method, *args)
     arg = args.first unless args.first.is_a?(Hash)
-    uri = "http://#{@server}/#{method}"
+    uri = "http://#{@server}#{method}"
     #uri += "/#{arg}" if arg
 
     Rails.logger.debug "[DEBUG] Invocando URL con los datos: " + uri
