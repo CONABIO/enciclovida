410c554f0b4743742f0eec427063030ce6eccc98
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2018-08-29 21:13:30 -0500

    fix a usuarios especie para la admin de comentarios para usuarios con taxa especifica

--- app/models/usuarios/usuario_especie.rb
+++ app/models/usuarios/usuario_especie.rb
@@ -2,12 +2,14 @@ class UsuarioEspecie < ActiveRecord::Base
 
   self.table_name = "#{CONFIG.bases.ev}.usuarios_especie"
 
+  alias_attribute :nombre_cientifico, :NombreCompleto
+
   belongs_to :usuario
   belongs_to :especie, :class_name=> 'Especie'
 
-  scope :join_usuarios,-> { joins('JOIN usuarios on usuario_id = usuarios.id') }
-  scope :join_especies,-> { joins('JOIN especies on especie_id = especies.id') }
-  scope :select_para_joins, -> { select("usuarios_especie.id, usuario_id, usuarios.nombre, usuarios.apellido, usuarios.email, usuarios.institucion, especie_id, especies.nombre_cientifico")}
+  scope :join_usuarios,-> { joins('JOIN enciclovida.usuarios on usuario_id = usuarios.id') }
+  scope :join_especies,-> { joins("JOIN #{Especie.table_name} on especie_id = #{Especie.attribute_alias(:id)}") }
+  scope :select_para_joins, -> { select("usuarios_especie.id, usuario_id, usuarios.nombre, usuarios.apellido, usuarios.email, usuarios.institucion, especie_id, #{Especie.attribute_alias(:nombre_cientifico)}")}
   scope :join_user_especies,-> { select_para_joins.join_usuarios.join_especies }
 
 end
