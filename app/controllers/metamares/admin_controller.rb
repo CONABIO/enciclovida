d304a674cefd66e4a95ee155e7e5019f7efae258
Jesus Alanis <jefealma@ciencias.unam.mx>
2018-09-10 01:28:49 -0500

    rutas, controladores y vistas para el admin de metamares

---
+++ app/controllers/metamares/admin_controller.rb
@@ -0,0 +1,76 @@
+class Metamares::AdminController < ApplicationController
+  before_action :authenticate_usuario!
+  before_action  do
+    tiene_permiso?('AdminMetamares')  # Minimo administrador
+  end
+
+  def index
+    @usuarios = Usuario.select(:id, :nombre, :apellido, :email).select('nombre_rol').left_joins(:usuario_roles, :roles).
+        where('nombre_rol=?', 'AdminMetamares').uniq
+  end
+
+  # GET /estatuses/1
+  # GET /estatuses/1.json
+  def show
+  end
+
+  # GET /estatuses/new
+  def new
+    @estatuse = Estatus.new
+  end
+
+  # GET /estatuses/1/edit
+  def edit
+  end
+
+  # POST /estatuses
+  # POST /estatuses.json
+  def create
+    @estatuse = Estatus.new(estatuse_params)
+
+    respond_to do |format|
+      if @estatuse.save
+        format.html { redirect_to @estatuse, notice: 'Estatus was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @estatuse }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @estatuse.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /estatuses/1
+  # PATCH/PUT /estatuses/1.json
+  def update
+    respond_to do |format|
+      if @estatuse.update(estatuse_params)
+        format.html { redirect_to @estatuse, notice: 'Estatus was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @estatuse.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /estatuses/1
+  # DELETE /estatuses/1.json
+  def destroy
+    @estatuse.destroy
+    respond_to do |format|
+      format.html { redirect_to estatuses_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_estatuse
+      @estatuse = Estatus.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def estatuse_params
+      params[:estatuse]
+    end
+end
