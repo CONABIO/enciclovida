5c1f43440f48c21af9fdced1b6d1d465e01ceca9
Jesus Alanis <jefealma@ciencias.unam.mx>
2018-09-14 19:56:56 -0500

    form de proyectos para tenerlo mas separado

--- app/controllers/metamares/proyectos_controller.rb
+++ app/controllers/metamares/proyectos_controller.rb
@@ -1,5 +1,7 @@
-class Metamares::FrontController < ApplicationController
+class Metamares::ProyectosController < ApplicationController
 
+  layout false
+  
   def index
     @proyectos = Metamares::Proyecto.all
   end
@@ -8,7 +10,7 @@ class Metamares::FrontController < ApplicationController
   end
 
   def new
-    @estatuse = Estatus.new
+    @proyecto = Metamares::Proyecto.new
   end
 
   def edit
