31882a6cee6659f30bbe216a0fc7873d6c1f6efe
Jesus Alanis <jefealma@ciencias.unam.mx>
2018-09-14 19:18:41 -0500

    renombrando region a region_m para evitar pex

--- app/models/metamares/proyecto.rb
+++ app/models/metamares/proyecto.rb
@@ -4,7 +4,7 @@ class Metamares::Proyecto < ActiveRecord::Base
 
   belongs_to :info_adicional, class_name: 'Metamares::InfoAdicional'
   belongs_to :periodo, class_name: 'Metamares::Periodo'
-  belongs_to :region, class_name: 'Metamares::Region'
+  belongs_to :region, class_name: 'Metamares::RegionM'
   belongs_to :institucion, class_name: 'Metamares::Institucion'
   belongs_to :dato, class_name: 'Metamares::Dato'
   belongs_to :usuario, class_name: 'Usuario'
