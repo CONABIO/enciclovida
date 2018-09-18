6c8165881d491acb1f4fcde21e52e962b1d5c171
Jesus Alanis <jefealma@ciencias.unam.mx>
2018-09-18 18:56:16 -0500

    formularios de dato

--- app/models/metamares/proyecto.rb
+++ app/models/metamares/proyecto.rb
@@ -5,8 +5,8 @@ class Metamares::Proyecto < ActiveRecord::Base
   belongs_to :info_adicional, class_name: 'Metamares::InfoAdicional'
   belongs_to :periodo, class_name: 'Metamares::Periodo'
   belongs_to :region, class_name: 'Metamares::RegionM'
-  belongs_to :institucion, class_name: 'Metamares::Institucion'
   belongs_to :dato, class_name: 'Metamares::Dato'
+  belongs_to :institucion, class_name: 'Metamares::Institucion'
   belongs_to :usuario, class_name: 'Usuario'
 
   accepts_nested_attributes_for :info_adicional, reject_if: :all_blank, allow_destroy: true
