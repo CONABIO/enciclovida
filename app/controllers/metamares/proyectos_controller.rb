d77cb8e16e667e1c57e1955e25c76dfe3b1142f8
Jesus Alanis <jefealma@ciencias.unam.mx>
2018-09-21 16:39:36 -0500

    los parametros en el controller

--- app/controllers/metamares/proyectos_controller.rb
+++ app/controllers/metamares/proyectos_controller.rb
@@ -34,8 +34,8 @@ class Metamares::ProyectosController < Metamares::MetamaresController
 
   def update
     respond_to do |format|
-      if @estatuse.update(estatuse_params)
-        format.html { redirect_to @estatuse, notice: 'Estatus was successfully updated.' }
+      if @proyecto.update(proyecto_params)
+        format.html { redirect_to @proyecto, notice: 'El proyecto fue actualizado satisfactoriamente.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -65,5 +65,26 @@ class Metamares::ProyectosController < Metamares::MetamaresController
   end
 
   def proyecto_params
+    params.require(:metamares_proyecto).permit(:nombre_proyecto, :financiamiento, :tipo_monitoreo, :objeto_monitoreo,
+                                               :campo_investigacion, :campo_ciencia, :finalidad, :metodo, :usuario_id,
+                                               info_adicional_attributes: [:id, :informacion_objeto, :informacion_posterior,
+                                                                           :informacion_adicional, :colaboradores,
+                                                                           :instituciones_involucradas, :equipo, :comentarios, :_destroy],
+                                               periodo_attributes: [:id, :periodicidad, :periodo_monitore_desde, :periodo_monitore_hasta,
+                                                                    :periodo_sistematico_desde, :periodo_sistematico_hasta,
+                                                                    :monitoreo_desde, :monitoreo_hasta, :comentarios, :_destroy],
+                                               region_attributes: [:id, :nombre_region, :latitud, :longitud, :poligono, :zona,
+                                                                   :entidad, :cuenca, :anp, :comentarios, :destroy],
+                                               dato_attributes: [:id, :descarga_datos, :licencia_uso, :descripcion_base,
+                                                                 :metadatos, :publicaciones, :publicacion_url, :descarga_informe,
+                                                                 :forma_citar, :notas_adicionales, :restricciones,
+                                                                 :numero_ejemplares, :tipo_unidad, :destroy],
+                                               institucion_attributes: [:id, :nombre_institucion, :sitio_web, :contacto,
+                                                                        :correo, :destroy],
+                                               ubicacion_attributes: [:id, :calle_numero, :colonia, :municipio, :ciudad,
+                                                                      :entidad_federativa, :cp, :pais, :destroy],
+                                               especies_attributes: [:id, :especie_id, :nombre_cientifico, :proyecto_id, :destroy],
+                                               keywords_attributes: [:id, :nombre_keyword, :proyecto_id, :destroy]
+                                               )
   end
 end
