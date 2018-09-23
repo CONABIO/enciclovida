0111cb20de77c419002fed948f5fb2b655e07d55
Jesus Alanis <jefealma@ciencias.unam.mx>
2018-09-23 00:29:23 -0500

    relacion especie desde EspecieEstudiada

--- app/models/metamares/especie_estudiada.rb
+++ app/models/metamares/especie_estudiada.rb
@@ -3,5 +3,6 @@ class Metamares::EspecieEstudiada < ActiveRecord::Base
   self.table_name = "#{CONFIG.bases.metamares}.especies_estudiadas"
 
   belongs_to :proyecto, class_name: 'Metamares::Proyecto'
+  belongs_to :especie, class_name: 'Especie'
 
 end
