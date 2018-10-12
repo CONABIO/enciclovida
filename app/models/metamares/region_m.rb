845eb58e7df83a5d021fbfe04ec418d312eaf3e3
Jesus Alanis <jefealma@ciencias.unam.mx>
2018-10-12 00:16:27 -0500

    input select para los campos de region_m

--- app/models/metamares/region_m.rb
+++ app/models/metamares/region_m.rb
@@ -4,5 +4,5 @@ class Metamares::RegionM < ActiveRecord::Base
 
   REGIONES = ['B. Camp. Caribe'.to_sym, 'Central Pacific'.to_sym, 'Freshwater/Terrestrial'.to_sym,
               'G. California'.to_sym, 'South Pacific'.to_sym, 'W. G. Mexico'.to_sym]
-
+  ZONAS = ['Atlantic'.to_sym, 'Freshwater/Terrestrial'.to_sym, 'National'.to_sym, 'Pacific'.to_sym]
 end
