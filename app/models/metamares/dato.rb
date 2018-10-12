f4b53bdf4ae86f142c4532adf6845846cba79d05
Jesus Alanis <jefealma@ciencias.unam.mx>
2018-10-12 03:42:55 -0500

    cambios para el estatus_datos

--- app/models/metamares/dato.rb
+++ app/models/metamares/dato.rb
@@ -2,4 +2,6 @@ class Metamares::Dato < ActiveRecord::Base
 
   self.table_name = "#{CONFIG.bases.metamares}.datos"
 
+  ESTATUS_DATOS = ['Public access'.to_sym, 'Restricted access'.to_sym, 'Private access'.to_sym]
+
 end
