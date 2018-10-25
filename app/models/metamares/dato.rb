4e41ea36a9595bd8d49c84481446767893d12bbf
Jesus Alanis <jefealma@ciencias.unam.mx>
2018-10-25 17:58:55 -0500

    cambios en algunos titulos y inks

--- app/models/metamares/dato.rb
+++ app/models/metamares/dato.rb
@@ -3,5 +3,6 @@ class Metamares::Dato < ActiveRecord::Base
   self.table_name = "#{CONFIG.bases.metamares}.datos"
 
   ESTATUS_DATOS = ['Public access'.to_sym, 'Restricted access'.to_sym, 'Private access'.to_sym]
+  CREATIVE_COMMONS = [:cc0, :cc_by, :cc_by_nc, :cc_by_sa, :cc_by_nd, :cc_by_nc_sa, :cc_by_nc_nd, :no_licence]
 
 end
