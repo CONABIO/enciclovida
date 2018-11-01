13dbe84cfb286820c0a43521cafd6e8560a83d08
Jesus Alanis <jefealma@ciencias.unam.mx>
2018-11-01 11:55:52 -0600

    cambios minimos

--- app/models/metamares/region_m.rb
+++ app/models/metamares/region_m.rb
@@ -3,6 +3,6 @@ class Metamares::RegionM < ActiveRecord::Base
   self.table_name = "#{CONFIG.bases.metamares}.regiones"
 
   REGIONES = ['B. Camp. Caribe'.to_sym, 'Central Pacific'.to_sym, 'Freshwater/Terrestrial'.to_sym,
-              'G. California'.to_sym, 'South Pacific'.to_sym, 'W. G. Mexico'.estandariza.to_sym]
+              'G. California'.to_sym, 'South Pacific'.to_sym, 'W. G. Mexico'.to_sym]
   ZONAS = ['Atlantic'.to_sym, 'Freshwater/Terrestrial'.to_sym, 'National'.to_sym, 'Pacific'.to_sym]
 end
