ec7f6fcd1f72d26631831fac3189e53fb9311d23
Jesus Alanis <jefealma@ciencias.unam.mx>
2018-11-22 22:28:27 -0600

    bug cuando se trata de crear nueva asociacion con ubicacion

--- app/models/metamares/institucion.rb
+++ app/models/metamares/institucion.rb
@@ -2,7 +2,7 @@ class Metamares::Institucion < ActiveRecord::Base
 
   self.table_name = "#{CONFIG.bases.metamares}.instituciones"
 
-  belongs_to :ubicacion, class_name: 'Metamares::Ubicacion'
-  has_one :proyecto, class_name: 'Metamares::Proyecto'
+  belongs_to :ubicacion, class_name: 'Metamares::Ubicacion', inverse_of: :institucion
+  has_one :proyecto, class_name: 'Metamares::Proyecto', inverse_of: :institucion
 
 end
--- app/models/metamares/ubicacion.rb
+++ app/models/metamares/ubicacion.rb
@@ -2,7 +2,8 @@ class Metamares::Ubicacion < ActiveRecord::Base
 
   self.table_name = "#{CONFIG.bases.metamares}.ubicaciones"
 
-  has_one :institucion, class_name: 'Metamares::Institucion'
-  has_many :proyectos, through: :institucion, source: :proyecto, inverse_of: :ubicacion
+  has_one :institucion, class_name: 'Metamares::Institucion', inverse_of: :ubicacion
+  has_many :proyectos, through: :institucion, source: :proyecto
 
+  validates_presence_of :institucion
 end
