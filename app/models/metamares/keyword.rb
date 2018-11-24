c614554198f7bb645adc50ba641f66d034e79502
Jesus Alanis <jefealma@ciencias.unam.mx>
2018-11-24 03:42:50 -0600

    nombre_keyword, ahora requerido

--- app/models/metamares/keyword.rb
+++ app/models/metamares/keyword.rb
@@ -3,5 +3,6 @@ class Metamares::Keyword < ActiveRecord::Base
   self.table_name = "#{CONFIG.bases.metamares}.keywords"
 
   belongs_to :proyecto, class_name: 'Metamares::Proyecto'
+  validates_presence_of :nombre_keyword
 
 end
