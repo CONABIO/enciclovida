b39da80ca10b6220d660989719e1b243d296bfd3
Jesus Alanis <jefealma@ciencias.unam.mx>
2018-11-24 02:10:53 -0600

    ligando meausuario a directorio

--- app/models/metamares/directorio.rb
+++ app/models/metamares/directorio.rb
@@ -2,7 +2,7 @@ class Metamares::Directorio < ActiveRecord::Base
 
   self.table_name = "#{CONFIG.bases.metamares}.directorio"
 
-  belongs_to :usuario, class_name: 'Usuario'
+  belongs_to :metausuario, class_name: 'Metausuario'
   belongs_to :institucion, class_name: 'Metamares::Institucion'
 
 end
--- app/models/metamares/metausuario.rb
+++ app/models/metamares/metausuario.rb
@@ -1,3 +1,11 @@
 class Metausuario < Usuario
 
+  has_one :directorio, :foreign_key => :usuario_id, :class_name=> 'Metamares::Directorio'
+
+  after_create :añade_directorio
+
+  def añade_directorio
+    directorio.new.save
+  end
+
 end
