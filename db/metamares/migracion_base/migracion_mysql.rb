127dd078f46ad8fdc31fb004a577641bb702bf88
Jesus Alanis <jefealma@ciencias.unam.mx>
2018-11-24 05:57:01 -0600

    bug en el nombre del proyecto

--- db/metamares/migracion_base/migracion_mysql.rb
+++ db/metamares/migracion_base/migracion_mysql.rb
@@ -24,7 +24,7 @@ def itera_metadata
 
     proyecto = Metamares::Proyecto.new
     proyecto.id = meta.mmid
-    proyecto.nombre_proyecto = meta.short_title
+    proyecto.nombre_proyecto = truncate(meta.short_title, length: 255)
     proyecto.autor = meta.author unless meta.author.estandariza == 'na'
     proyecto.financiamiento = meta.research_fund unless meta.research_fund.estandariza == 'na'
     proyecto.campo_investigacion = meta.research_field unless meta.research_field.estandariza == 'na'
