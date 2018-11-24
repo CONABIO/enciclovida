6bc555924223f02aca1c2e17a1a2c243fe6ed0ae
Jesus Alanis <jefealma@ciencias.unam.mx>
2018-11-24 05:54:37 -0600

    archivos no necesarios

--- app/models/metamares/proyecto_especie.rb
+++
@@ -1,8 +0,0 @@
-class Metamares::ProyectoEspecie < ActiveRecord::Base
-
-  self.table_name = "#{CONFIG.bases.metamares}.proyectos_especies"
-
-  belongs_to :proyecto, class_name: 'Metamares::Proyecto'
-  belongs_to :especie_estudiada, class_name: 'Metamares::EspecieEstudiada'
-
-end
