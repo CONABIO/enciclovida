45000b6d48841ab056c924e7728cd4efe2e62bb4
martihs <2123064604@alumnos.cua.uam.mx>
2018-12-04 02:19:35 -0600

    correción para que no corra fuzzy

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ PORT = CONFIG.site_url.split(':')[2][0..-1]
 SITE_NAME = CONFIG.site_name
 SITE_NAME_SHORT = CONFIG.site_name_short || SITE_NAME
 
-if Rails.env.development_mac?
+if !Rails.env.development_mac?
   require 'blurrily/client.rb'  #para el fuzzy match
   FUZZY_NOM_COM = Blurrily::Client.new(:host => IP, :db_name => 'nombres_comunes')
   FUZZY_NOM_CIEN = Blurrily::Client.new(:host => IP, :db_name => 'nombres_cientificos')
