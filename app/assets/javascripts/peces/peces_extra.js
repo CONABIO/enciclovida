94fe72d095e667b3a16eae31cd05b1f2bfc49b4b
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2018-12-14 19:14:19 -0600

    cambios sugeridos por @larpa

--- app/assets/javascripts/peces/peces_extra.js
+++ app/assets/javascripts/peces/peces_extra.js
@@ -2,9 +2,8 @@ var limpiaBusqueda = function(){
     var controles = ".porGrupo input, .porSemaforo input, .porZonas input, .porNombreGrupo input, .porNombreGrupo select, .porCriterios input, .porCriterios select";
     var visuales = ".porGrupo span, .porSemaforo span, .porZonas span, .porZonas path, .porCriterios span";
     var inputsALimpiar = $(controles + ", " + visuales);
-    inputsALimpiar.attr("disabled", false).removeClass("disabled zona-seleccionada");
+    $('input, select, span, path').removeClass("disabled zona-seleccionada");
     $("#id, #nombre").val('');
-    inputsALimpiar.prop("checked", false);
 };
 
 var bloqueaBusqueda = function(){
@@ -12,9 +11,7 @@ var bloqueaBusqueda = function(){
     var visuales = ".porGrupo span, .porSemaforo span, .porZonas span, .porZonas path, .porCriterios span";
     var inputsABloquear = $(controles + ", " + visuales);
     inputsABloquear.attr("disabled", true).addClass("disabled");
-    //inputsABloquear.val('');
     inputsABloquear.prop("checked", false);
-
 };
 
 $(document).ready(function(){
