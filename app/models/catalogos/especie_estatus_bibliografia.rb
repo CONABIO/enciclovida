87b12344a68762daf6f8c2210c79327a4243fb16
Carlos Alonso <calonsogeek@gmail.com>
2019-01-21 19:46:42 -0600

    nueva tabla, RelacionBibliografia, para sacar las bibliografias de sinonimos, homonimos, etc

--- app/models/catalogos/especie_estatus_bibliografia.rb
+++ app/models/catalogos/especie_estatus_bibliografia.rb
@@ -1,20 +1,18 @@
-class EspecieEstatus < ActiveRecord::Base
+class EspecieEstatusBibliografia < ActiveRecord::Base
 
-  self.table_name = "#{CONFIG.bases.cat}.Nombre_Relacion"
-  self.primary_keys= :IdNombre, :IdNombreRel, :IdTipoRelacion
+  self.table_name = "#{CONFIG.bases.cat}.RelacionBibliografia"
+  self.primary_keys= :IdNombre, :IdNombreRel, :IdTipoRelacion, :IdBibliografia
 
   # Los alias con las tablas de catalogos
   alias_attribute :especie_id1, :IdNombre
   alias_attribute :especie_id2, :IdNombreRel
   alias_attribute :estatus_id, :IdTipoRelacion
+  alias_attribute :bibliografia_id, :IdBibliografia
   alias_attribute :observaciones, :Observaciones
 
   belongs_to :especie, :foreign_key => attribute_alias(:especie_id1)
   belongs_to :especie, :foreign_key => attribute_alias(:especie_id2)
   belongs_to :estatus, :foreign_key => attribute_alias(:estatus_id)
-  has_many :bibliografias, class_name: 'EspecieEstatusBibliografia', :foreign_key => attribute_alias(:especie_id1)
-
-  scope :sinonimos, -> { where(estatus_id: 1) }
-  scope :homonimos, -> { where(estatus_id: 8) }
+  belongs_to :bibliografia, :foreign_key => attribute_alias(:bibliografia_id)
 
 end
