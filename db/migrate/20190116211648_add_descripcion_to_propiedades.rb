d5fdc4ea74407ec4f82ee6501371fee8daee5e6d
Carlos Alonso <calonsogeek@gmail.com>
2019-02-05 18:15:09 -0600

    nueva migracion para poner valor default a valor_total

--- db/migrate/20190116211648_add_descripcion_to_propiedades.rb
+++ db/migrate/20190116211648_add_descripcion_to_propiedades.rb
@@ -1,8 +1,4 @@
 class AddDescripcionToPropiedades < ActiveRecord::Migration[5.1]
-  def conexion_a_pmc
-    @connection = ActiveRecord::Base.establish_connection(CONFIG.bases.pez.to_sym).connection
-  end
-
   def up
     add_column "#{CONFIG.bases.pez}.propiedades", :descripcion, :text
   end
