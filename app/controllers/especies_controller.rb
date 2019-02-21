d8b220366cadabeff0477dac8039cb7fe5d57ae5
Alberto González <albertoglezba@gmail.com>
2019-02-21 03:17:17 -0600

    parche para que jale el cahcce de las fichas diferente a conabio ¬¬

--- app/controllers/especies_controller.rb
+++ app/controllers/especies_controller.rb
@@ -27,8 +27,7 @@ class EspeciesController < ApplicationController
                           :cambia_id_naturalista, :dame_nombre_con_formato, :noticias]
 
   # Pone en cache el webservice que carga por default
-  caches_action :describe, :expires_in => eval(CONFIG.cache.fichas),
-                :cache_path => Proc.new { |c| "especies/#{c.params[:id]}/#{c.params[:from]}" } if Rails.env.production? && (c.params[:from].present? && c.params[:from] != 'Conabio')
+  caches_action :describe, :expires_in => eval(CONFIG.cache.fichas), :cache_path => Proc.new { |c| "especiesR/#{c.params[:id]}/#{c.params[:from]}" }, :if => (Rails.env.production? && :params_from_conabio_present?)
 
   # GET /especies
   # GET /especies.json
@@ -926,5 +925,11 @@ class EspeciesController < ApplicationController
     when 5
       "caso_rango_valores('#{columna}', \"#{valor}\")"
     end
+  end
+
+
+  # Este método es necesario para ver params antes de que se inicialice dicha variable (caches_action corre antes q eso)
+  def params_from_conabio_present?
+    params.present? && params[:from].present? && params[:from] != 'Conabio'
   end
 end
