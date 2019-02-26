6f1dbe0ac8cd30a1a9df8b596f85eba69daf610e
Carlos Alonso <calonsogeek@gmail.com>
2019-02-26 17:57:43 -0600

    bug en la ruta y algunas convenciones, mio

---
+++ app/lib/taxon_describers/bhl.rb
@@ -0,0 +1,17 @@
+module TaxonDescribers
+
+  class Bhl < Base
+
+    def self.describer_name
+      'BHL'
+    end
+
+    def self.describe(taxon)
+      url = "#{CONFIG.site_url}especies/#{taxon.id}/bhl"
+      Rails.logger.debug "[DEBUG] Buscando con: #{url}"
+      escaped_address = URI.escape(url)
+      uri = URI.parse(escaped_address)
+      Nokogiri::HTML(open(uri))
+    end
+  end
+end
