f55d66a028a5929c27d888f248267298a394b5a2
Jesus Alanis <jefealma@ciencias.unam.mx>
2019-02-25 20:09:37 -0600

    bugs y cambios en las consultas

--- app/models/metamares/directorio.rb
+++ app/models/metamares/directorio.rb
@@ -2,7 +2,6 @@ class Metamares::Directorio < MetamaresAbs
 
   self.table_name = "#{CONFIG.bases.metamares}.directorio"
 
-  belongs_to :metausuario, class_name: 'Metausuario'
-  belongs_to :institucion, class_name: 'Metamares::Institucion'
+  validates_uniqueness_of :usuario_id
 
 end
