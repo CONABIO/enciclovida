50cdf0a7297147c40d8faf5c7549ec9ca8000d6d
Carlos Alonso <calonsogeek@gmail.com>
2019-03-19 19:43:53 -0600

    municipios en redis

--- app/models/geoportal/municipio.rb
+++ app/models/geoportal/municipio.rb
@@ -8,4 +8,31 @@ class Geoportal::Municipio < GeoportalAbs
   scope :campos_geom, -> { centroide.geojson_select }
   scope :geojson, ->(region_id, parent_id) { geojson_select.where(cve_mun: region_id, cve_ent: parent_id) }
 
+
+  private
+
+  def nombre_publico
+    "#{municipio}, #{I18n.t("estados.#{estado.estandariza.gsub('-', '_')}")}"
+  end
+
+  def asigna_redis
+    asigna_redis_id
+    self.redis[:data] = {}
+    self.redis[:term] = municipio.limpia.downcase
+    self.redis[:score] = 100
+    self.redis[:data][:id] = munid
+    self.redis[:data][:nombre] = nombre_publico
+
+    redis.deep_stringify_keys!
+  end
+
+  # Arma el ID de redis
+  def asigna_redis_id
+    # El 2 inicial es para saber que es un region
+    # El 1 en la segunda posicion denota que es un estado
+    # Y despues se ajusta a 8 digitos el numero de estado, para dar un total de 10 digitos
+    self.redis = {} unless redis.present?
+    self.redis["id"] = "21#{munid.to_s.rjust(8,'0')}".to_i
+  end
+
 end
