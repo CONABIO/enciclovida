3a7f13e34e60e8703313c34b4fc14f6c5c87b495
Marti <2123064604@alumnos.cua.uam.mx>
2019-03-20 22:39:17 -0600

    cambios subidos por @albertoglezba, estos no estaban comiteados IDEM 20032019

--- app/controllers/fichas/admin_controller.rb
+++ app/controllers/fichas/admin_controller.rb
@@ -1,10 +1,12 @@
 class Fichas::AdminController < Fichas::FichasController
 
-
-
   def edit
     @form_params = { url: '/fichas/admin', method: 'post' }
     @taxon = Fichas::Taxon.find(1)
+  end
+
+  def show
+
   end
 
 end
