169f23f34a08960f8f681f11954cb2fe3e822ded
Carlos Alonso <calonsogeek@gmail.com>
2019-03-25 19:04:58 -0600

    las regiones de topojson ahora se generan con rake tasks

--- lib/geo_a_topo.rb
+++ lib/geo_a_topo.rb
@@ -1,4 +1,39 @@
-class GeoATopo
+class GeoAtopo
+
+  # Guarda el topo json de la region seleccionada, posibles valores son: estado municipio anp ecorregion
+  def topojson_por_region(region)
+    return nil unless %w(estado municipio anp ecorregion).include?(region)
+    Rails.logger.debug "[DEBUG] - Generando los topojson con region: #{region}"
+
+    ruta = Rails.root.join('public', 'topojson')
+    Dir.mkdir(ruta) unless File.exists?(ruta)
+    geojson_todos = {type: 'FeatureCollection', features: []}  # Para todos loes estados o municipios juntos
+
+    "Geoportal::#{region.camelize}".constantize.campos_min.campos_geom.all.each do |reg|
+      Rails.logger.debug "[DEBUG]Generando la región: #{reg.nombre_publico}"
+
+      geojson = {type: 'FeatureCollection', features: []}
+      feature = {type: 'Feature', properties:{region_id: reg.region_id, centroide: [reg.lat, reg.long]}, geometry: JSON.parse(reg.geojson)}
+      feature[:properties][:nombre_region] = reg.nombre_publico
+      feature[:properties][:tipo] = reg.tipo
+      geojson[:features] << feature
+      geojson_todos[:features] << feature
+
+    end  # End cada region each
+
+    archivo_topo_todos = ruta.join("#{region}.json")
+    archivo_geo_todos = ruta.join('collection.json')
+    archivo_tmp_todos = ruta.join("#{region}_tmp.json")
+
+    # Escribe a disco el archivo geojson
+    File.write(archivo_geo_todos, geojson_todos.to_json)
+
+    # Convierte a topojson
+    topojson_todos = dame_topojson_system({q: '1e4', p: '7e-8', i: archivo_geo_todos, o: archivo_topo_todos, tmp: archivo_tmp_todos})
+    File.delete(archivo_geo_todos) if File.exist?(archivo_geo_todos)
+    Rails.logger.debug "[DEBUG] - Hubo un error al generar la region: #{archivo_topo_todos}" unless topojson_todos
+
+  end
 
   # Regresa en formato topojson
   def dame_topojson(collection)
--- lib/tasks/geoportal.rake
+++ lib/tasks/geoportal.rake
@@ -1,27 +1,37 @@
+require "#{Rails.root}/lib/geo_a_topo.rb"
 namespace :geoportal do
 
-  desc "Genera/actualiza los estados de la base del geoportal"
+  desc "Genera/actualiza los estados de la base del geoportal y crea el mapa con el topojson"
   task estados: :environment do
     Geoportal::Estado.all.each do |estado|
       estado.borra_redis
       estado.guarda_redis
     end
+
+    geo = GeoAtopo.new
+    geo.topojson_por_region('estado')
   end
 
-  desc "Genera/actualiza los municipios de la base del geoportal"
+  desc "Genera/actualiza los municipios de la base del geoportal y crea el mapa con el topojson"
   task municipios: :environment do
     Geoportal::Municipio.all.each do |municipio|
       municipio.borra_redis
       municipio.guarda_redis
     end
+
+    geo = GeoAtopo.new
+    geo.topojson_por_region('municipio')
   end
 
-  desc "Genera/actualiza las ANPs de la base del geoportal"
+  desc "Genera/actualiza las ANPs de la base del geoportal y crea el mapa con el topojson"
   task anps: :environment do
     Geoportal::Anp.all.each do |anp|
       anp.borra_redis
       anp.guarda_redis
     end
+
+    geo = GeoAtopo.new
+    geo.topojson_por_region('anp')
   end
 
 end
