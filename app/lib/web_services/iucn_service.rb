9b2d726045cd57c1e2e03865966dbc5e3e0174ac
Carlos Alonso <calonsogeek@gmail.com>
2019-04-29 18:05:57 -0500

    archivos basicos para actualizar con IUCN

--- app/lib/web_services/iucn_service.rb
+++ app/lib/web_services/iucn_service.rb
@@ -1,5 +1,8 @@
 class IUCNService
 
+  attr_accessor :csv_path
+
+  # Consulta la categoria de riesgo de un taxon dado
   def consultaRiesgo(opts)
     @iucn = CONFIG.iucn.api
     @token = CONFIG.iucn.token
@@ -17,6 +20,7 @@ class IUCNService
     end
   end
 
+  # Guarda en cache la respuesta del servicio
   def dameRiesgo(opc={})
     resp = Rails.cache.fetch("iucn_#{opc[:id]}", expires_in: eval(CONFIG.cache.iucn)) do
       iucn = consultaRiesgo(opc)
@@ -24,6 +28,25 @@ class IUCNService
     end
 
     resp
+  end
+
+  # Accede al archivo que contiene los assessments y la taxonomia dentro de la carpeta versiones_IUCN
+  # NOTAS: Este archivo se baja de la pagina de IUCN y hay que unir el archivo de asswessments con el de taxonomy
+  def lee_csv(archivo)
+    self.csv_path = Rails.root.join('public', 'IUCN', archivo)
+    Rails.logger.debug "[DEBUG] - Corriendo con archivo: #{csv_path}"
+    return unless File.exists? csv_path
+
+    CSV.foreach(csv_path, :headers => true) do |row|
+      puts row.inspect + '-----------'
+      return
+    end
+  end
+
+  # Bitacora especial para catalogos, antes de correr en real, pasarsela
+  def bitacora
+    log_path = Rails.root.join('log', Time.now.strftime('%Y-%m-%d_%H%m') + '_IUCN.log')
+    @@bitacora ||= Logger.new(log_path)
   end
 
 end
