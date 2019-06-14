a171d788d1efcabd45ebdfc977493c045d49329c
Carlos Alonso <calonsogeek@gmail.com>
2019-06-13 21:22:57 -0500

    quitando zona y ordenando las regiones en los filtros - infoceanos

--- app/models/metamares/region_m.rb
+++ app/models/metamares/region_m.rb
@@ -2,7 +2,6 @@ class Metamares::RegionM < MetamaresAbs
 
   self.table_name = "#{CONFIG.bases.metamares}.regiones"
 
-  REGIONES = ['B Camp Caribe'.to_sym, 'Central Pacific'.to_sym, 'Freshwater_Terrestrial'.to_sym,
-              'G California'.to_sym, 'South Pacific'.to_sym, 'W G Mexico'.to_sym]
+  REGIONES = ['G California'.to_sym, 'Central Pacific'.to_sym, 'South Pacific'.to_sym, 'W G Mexico'.to_sym, 'B Camp Caribe'.to_sym, 'Freshwater_Terrestrial'.to_sym]
   ZONAS = ['Atlantic'.to_sym, 'Freshwater/Terrestrial'.to_sym, 'National'.to_sym, 'Pacific'.to_sym]
 end
