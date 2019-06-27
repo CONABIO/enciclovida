815635cb2d021fab85de9036dbe942831573ede8
martihs <2123064604@alumnos.cua.uam.mx>
2019-06-27 15:41:38 -0500

    Más opciones en modelos

--- app/models/fichas/historianatural.rb
+++ app/models/fichas/historianatural.rb
@@ -18,16 +18,119 @@ class Fichas::Historianatural < Ficha
 	accepts_nested_attributes_for :reproduccionVegetal, allow_destroy: true
 
 	FUNCIONES_ECOLOGICAS = [
-			'Productores'.to_sym,
-			'Depredador'.to_sym,
-			'Depredador tope'.to_sym,
-			'Descomponedor'.to_sym,
-			'Dispersor'.to_sym,
-			'Polinizador'.to_sym,
-			'Fijadores de carbono'.to_sym,
-			'Fijadores de nitrógeno'.to_sym,
-			'Otros'.to_sym
+    'Productores'.to_sym,
+    'Depredador'.to_sym,
+    'Depredador tope'.to_sym,
+    'Descomponedor'.to_sym,
+    'Dispersor'.to_sym,
+    'Polinizador'.to_sym,
+    'Fijadores de carbono'.to_sym,
+    'Fijadores de nitrógeno'.to_sym,
+    'Otros'.to_sym
 	]
+
+	HABITO_PLANTAS = [
+    'Árboles'.to_sym,
+    'Arbustos'.to_sym,
+    'Subarbustos'.to_sym,
+    'Hierbas'.to_sym,
+    'Lianas'.to_sym,
+    'Suculentas'.to_sym,
+    'En forma de almohada'.to_sym,
+    'Epífitas'.to_sym,
+    'Arrosetadas'.to_sym,
+    'Trepadoras'.to_sym,
+    'Rastreras'.to_sym,
+    'Parásitas'.to_sym,
+    'Arborescente'.to_sym
+	]
+
+	ALIMENTACION = [
+		'Autótrofos'.to_sym,
+		'Heterótrofos'.to_sym,
+		'Herbívoros(Polinívoros)'.to_sym,
+		'Herbívoros(Nectarívoros)'.to_sym,
+		'Herbívoros(Granívoros)'.to_sym,
+		'Herbívoros(Frugívoros)'.to_sym,
+		'Herbívoros(Folívoros)'.to_sym,
+		'Herbívoros(Rizófagos)'.to_sym,
+		'Carnívoros(Hematófagos)'.to_sym,
+		'Carnívoros(Carroñeros)'.to_sym,
+		'Carnívoros(Insectívoros)'.to_sym,
+		'Carnívoros(Piscívoros)'.to_sym,
+		'Carnívoros(Oófagos)'.to_sym,
+		'Omnívoros'.to_sym,
+		'Detritívoros'.to_sym,
+		'Saprófagos'.to_sym,
+		'Carnívoros'.to_sym
+	]
+
+	FORRAJEO = [
+		'Acuático'.to_sym,
+		'Arbóreas'.to_sym,
+		'Buceador'.to_sym,
+		'Carroñero'.to_sym,
+		'Cazador aéreo (bajo el dosel)'.to_sym,
+		'Cazador aéreo (sobre el dosel)'.to_sym,
+		'Cazador terrestre'.to_sym,
+		'Forrajeo en el follaje'.to_sym,
+		'Forrajeo en el suelo'.to_sym,
+		'Forrajeo en la corteza de los árboles'.to_sym
+	]
+
+	ESTATUS_MIGRATORIO = [
+		'Residente'.to_sym,
+		'Migratorio de invierno'.to_sym,
+		'Transitorio'.to_sym,
+		'Accidental'.to_sym,
+		'Otro'.to_sym
+	]
+
+	TIPO_MIGRACION = [
+		'Latitudinal'.to_sym,
+		'Longitudinal'.to_sym,
+		'Altitudinal'.to_sym,
+		'Vertical en columnas de agua'.to_sym,
+		'Migrante local'.to_sym
+	]
+
+	HABITO = [
+		'Arborícola'.to_sym,
+		'Acuático'.to_sym,
+		'Cursorial'.to_sym,
+		'Epífito'.to_sym,
+		'Palustre'.to_sym,
+		'Rupícola'.to_sym,
+		'Saltatorial'.to_sym,
+		'Semiarborícola'.to_sym,
+		'Semiacuático'.to_sym,
+		'Terrestre'.to_sym,
+		'Trepador'.to_sym,
+		'Volador'.to_sym,
+		'Otro'.to_sym
+	]
+
+	PERIODO_ACTIVIDAD = [
+		'Diurno'.to_sym,
+		'Nocturno'.to_sym,
+		'Crepuscular'.to_sym
+	]
+
+	MECANISMO_DEFENSA = [
+		'Defensa química'.to_sym,
+		'Alelopatía'.to_sym,
+		'Coloración'.to_sym,
+		'Defensa química'.to_sym,
+		'Espinas'.to_sym,
+		'Mimetismo'.to_sym,
+		'Veneno'.to_sym
+	]
+
+  TIPO_REPRODUCCION = [
+    'animal'.to_sym,
+    'veget'.to_sym
+  ]
+
 
 	def get_info_reproduccion
 		if self.tipoReproduccion == 'animal'
--- app/models/fichas/reproduccionvegetal.rb
+++ app/models/fichas/reproduccionvegetal.rb
@@ -6,4 +6,70 @@ class Fichas::Reproduccionvegetal < Ficha
 	belongs_to :cat_caracfruto, :class_name => 'Fichas::Cat_Caracfruto', :foreign_key => 'IdFruto'
 	has_one :historiaNatural, class_name: 'Fichas::Historianatural'
 
+	FLORES = [
+		'Hermafroditas'.to_sym,
+		'Unisexuales'.to_sym
+	]
+
+	INDIVIDUOS = [
+		'Monoicas'.to_sym,
+		'Dioicas'.to_sym,
+		'Andromonoicas'.to_sym,
+		'Ginomonoicas'.to_sym,
+		'Subandroicas'.to_sym,
+		'Subginoicas'.to_sym,
+		'Polígamas'.to_sym
+	]
+
+	POBLACIONES = [
+		'Hermafroditas'.to_sym,
+		'Monoicas'.to_sym,
+		'Dioicas'.to_sym,
+		'Ginodioicas'.to_sym,
+		'Androdiocas'.to_sym,
+		'Trioicas'.to_sym
+	]
+
+	AISLAMIENTO_ORGANOS_REPROD = [
+		'Dicogamia'.to_sym,
+		'Protandria'.to_sym,
+		'Protoginia'.to_sym,
+		'Hercogamia'.to_sym
+	]
+
+	SISTEMAS_REPROD_ASEXUALES = [
+		'Multiplicación vegetativa'.to_sym,
+		'Esporulación'.to_sym,
+		'Apomixis'.to_sym
+	]
+
+	TIPO_FECUNDACION = [
+		'Alogamia'.to_sym,
+		'Autogamia'.to_sym,
+		'Cleistogamia'.to_sym
+	]
+
+	TIPO_POLINIZACION = [
+		'Polinización cruzada'.to_sym,
+		'Autopolinización'.to_sym
+	]
+
+	VECTORES_POLINIZACION = [
+		'Viento'.to_sym,
+		'Agua'.to_sym,
+		'Gravedad'.to_sym,
+		'Animales'.to_sym
+	]
+
+	FLORACION_HORARIO_APERTURA = [
+		'Diurno'.to_sym,
+		'Crepuscular'.to_sym,
+		'Nocturno'.to_sym
+	]
+
+	EVENTOS_REPROD = [
+		'Iteróparo'.to_sym,
+		'Semélparo'.to_sym
+	]
+
 end
