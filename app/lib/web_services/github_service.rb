9fd79d8f3cd711fb509cca04f32f18143050389f
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2019-07-22 20:51:33 -0500

    reemplazando puts por rails logger y limpieza

--- app/lib/web_services/github_service.rb
+++ app/lib/web_services/github_service.rb
@@ -2,7 +2,6 @@ class GithubService
 
   def dame_issues
     resp = Rails.cache.fetch('github_issues', expires_in: eval(CONFIG.cache.github.issues)) do
-      puts 'ENTRO A GENERAR'
       lee_issues
     end
 
