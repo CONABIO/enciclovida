6a691298292aa89de0593ce7089b8feb495aa19a
Carlos Alonso <calonsogeek@gmail.com>
2019-08-09 16:48:17 -0500

    primary_keys con cocoon

--- app/models/fichas/observacionescarac.rb
+++ app/models/fichas/observacionescarac.rb
@@ -1,11 +1,10 @@
 class Fichas::Observacionescarac < Ficha
 
 	self.table_name = "#{CONFIG.bases.fichasespecies}.observacionescarac"
-	self.primary_key = :especieId
+	self.primary_keys = :especieId, :idpregunta
 
   belongs_to :taxon, class_name: 'Fichas::Taxon', :foreign_key => 'especieId'
-	validates_uniqueness_of :especieId, :scope => :idpregunta
-
+  
 	PREGUNTAS = {
 			:info_ecorregiones => 52
 	}
