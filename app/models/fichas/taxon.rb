61a44a94b9115b7332d12e2c883943bfb335f992
martihs <2123064604@alumnos.cua.uam.mx>
2019-08-23 09:25:53 -0500

    Cambio de Taxón

--- app/models/fichas/taxon.rb
+++ app/models/fichas/taxon.rb
@@ -6,16 +6,22 @@ class Fichas::Taxon < Ficha
 	has_one :scat, class_name: 'Scat', primary_key: :IdCAT, foreign_key: Scat.attribute_alias(:catalogo_id)
 	has_one :especie, through: :scat, source: :especie
 	has_one :habitats, class_name: 'Fichas::Habitat', :foreign_key => 'especieId', inverse_of: :taxon
+
 	has_many :distribuciones, :class_name => 'Fichas::Distribucion', :foreign_key => 'especieId', inverse_of: :taxon
 	has_many :distribucion_historica, class_name: 'Fichas::Distribucionhistorica', :foreign_key => "especieId", inverse_of: :taxon
 	has_many :endemicas, :class_name => 'Fichas::Endemica', :foreign_key => 'especieId', inverse_of: :taxon
-
   has_one :historiaNatural, class_name: 'Fichas::Historianatural', :foreign_key => 'especieId', inverse_of: :taxon
   has_many :productocomercio_nal,-> {where('nacionalinternacional = "nacional"')}, class_name: 'Fichas::Productocomercio', :foreign_key => 'especieId', inverse_of: :taxon
   has_many :productocomercio_inter,-> {where('nacionalinternacional = "internacional"')}, class_name: 'Fichas::Productocomercio', :foreign_key => 'especieId', inverse_of: :taxon
   has_many :conservacion, :class_name => 'Fichas::Conservacion', :foreign_key => 'especieId', inverse_of: :taxon
   has_many :demografiaAmenazas, :class_name=> 'Fichas::Demografiaamenazas', :foreign_key => 'especieId', inverse_of: :taxon
   has_many :referenciasBibliograficas, class_name: 'Fichas::Referenciabibliografica', :foreign_key => 'especieId', inverse_of: :taxon
+	has_one :invasividad, class_name: 'Fichas::Invasividad', :foreign_key => 'especieId'
+	has_many :legislaciones, class_name: 'Fichas::Legislacion', :foreign_key => 'especieId'
+	has_many :metadatos, class_name: 'Fichas::Metadatos', :foreign_key => 'especieId'
+	has_one :nombreComun, class_name: 'Fichas::Nombrecomun', :foreign_key => 'especieId'
+	has_many :productoComercios, class_name: 'Fichas::Productocomercio', :foreign_key => 'especieId'
+	has_many :sinonimos , class_name: 'Fichas::Sinonimo', :foreign_key => 'especieId'
 
 
 	accepts_nested_attributes_for :habitats, allow_destroy: true
@@ -26,23 +32,6 @@ class Fichas::Taxon < Ficha
   accepts_nested_attributes_for :historiaNatural, allow_destroy: true
   accepts_nested_attributes_for :productocomercio_nal, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :productocomercio_inter, reject_if: :all_blank, allow_destroy: true
-
-
-	# - - - - - Pendientes:
-
-
-
-
-	has_one :invasividad, class_name: 'Fichas::Invasividad', :foreign_key => 'especieId'
-	has_many :legislaciones, class_name: 'Fichas::Legislacion', :foreign_key => 'especieId'
-	has_many :metadatos, class_name: 'Fichas::Metadatos', :foreign_key => 'especieId'
-	has_one :nombreComun, class_name: 'Fichas::Nombrecomun', :foreign_key => 'especieId'
-	has_many :productoComercios, class_name: 'Fichas::Productocomercio', :foreign_key => 'especieId'
-	has_many :sinonimos , class_name: 'Fichas::Sinonimo', :foreign_key => 'especieId'
-
-
-
-
 	accepts_nested_attributes_for :invasividad, allow_destroy: true
 	accepts_nested_attributes_for :endemicas, allow_destroy: true
 	accepts_nested_attributes_for :legislaciones, reject_if: :all_blank, allow_destroy: true
