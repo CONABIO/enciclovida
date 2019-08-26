64d0db640b834546796a84dbfdc8a5248cf7b716
Carlos Alonso <calonsogeek@gmail.com>
2019-08-26 18:22:12 -0500

    checklist con joins e includes, va quedando perron

--- app/models/catalogos/especie_region.rb
+++ app/models/catalogos/especie_region.rb
@@ -13,7 +13,7 @@ class EspecieRegion < ActiveRecord::Base
 
   belongs_to :region, :foreign_key => Region.attribute_alias(:id)
   belongs_to :especie
-  belongs_to :tipo_distribucion, :foreign_key => TipoDistribucion.attribute_alias(:id)
+  belongs_to :tipo_distribucion, -> { I18n.locale == 'es-cientifico' ? distribuciones_vista_especialistas : distribuciones_vista_general }, foreign_key: TipoDistribucion.attribute_alias(:id)
   has_many :nombres_regiones, :class_name => 'NombreRegion', :foreign_key => 'especie_id', :dependent => :destroy
 
 end
