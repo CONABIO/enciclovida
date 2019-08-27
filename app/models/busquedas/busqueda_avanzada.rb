5cb2f5f8d5f3a8c0028fe7a123a8745fba395669
Carlos Alonso <calonsogeek@gmail.com>
2019-08-27 18:53:18 -0500

    query completo, falta desplaegar en web

--- app/models/busquedas/busqueda_avanzada.rb
+++ app/models/busquedas/busqueda_avanzada.rb
@@ -57,7 +57,7 @@ class BusquedaAvanzada < Busqueda
 
   def checklist
     ids_checklist = taxones.select_ancestry.map{ |t| t.ancestry.split(',') }.flatten.uniq!
-    self.taxones = Especie.select_basico.left_joins(:categoria_taxonomica, :adicional).includes(:nombres_comunes, :tipos_distribuciones).checklist_con_filtros.where(id: ids_checklist)
+    self.taxones = Especie.select_basico.left_joins(:categoria_taxonomica, :adicional).includes(:nombres_comunes, :tipos_distribuciones, :catalogos, :bibliografias, :regiones).datos_checklist.where(id: ids_checklist)
 
     # TODO: si se pudiese hacer con un OR seria lo ideal
     #ancestros = taxones.select_ancestry.map{ |a| a.ancestry.split(',').delete_if { |x| x.blank? } }.flatten.uniq
--- app/models/catalogos/especie.rb
+++ app/models/catalogos/especie.rb
@@ -96,9 +96,9 @@ class Especie < ActiveRecord::Base
   # Select y joins basicos que contiene los campos a mostrar por ponNombreCientifico
   scope :datos_basicos, ->(attr_adicionales=[]) { select_basico(attr_adicionales).categoria_taxonomica_join.adicional_join }
   #Select para el Checklist
-  scope :select_checklist_con_filtros, -> { select(:id, :nombre_cientifico).select("CONCAT(#{attribute_alias(:ancestry_ascendente_directo)}, ',', #{attribute_alias(:nombre_cientifico)}) AS ancestry") }
-  scope :select_ancestry, -> { select(:id).select("#{attribute_alias(:ancestry_ascendente_directo)} AS ancestry") }
-  scope :checklist_con_filtros, -> { select_checklist_con_filtros.order('ancestry ASC') }
+  scope :select_checklist, -> { select(:id, :nombre_cientifico).select("CONCAT(#{attribute_alias(:ancestry_ascendente_obligatorio)}, ',', #{attribute_alias(:nombre_cientifico)}) AS ancestry") }
+  scope :select_ancestry, -> { select(:id).select("#{attribute_alias(:ancestry_ascendente_obligatorio)} AS ancestry") }
+  scope :datos_checklist, -> { select_checklist.order('ancestry ASC') }
   scope :datos_arbol_sin_filtros, -> {select("especies.id, nombre_cientifico, ancestry_ascendente_directo,
 ancestry_ascendente_directo+'/'+cast(especies.id as nvarchar) as arbol, categoria_taxonomica_id,
 categorias_taxonomicas.nombre_categoria_taxonomica, nombre_autoridad, estatus, nombre_comun_principal,
