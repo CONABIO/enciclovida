07539edc3951d4d1f1254e93840efb94fa5325d9
Carlos Alonso <calonsogeek@gmail.com>
2019-11-05 22:53:29 -0600

    relacion has_many :especies

--- app/models/admin/catalogos/catalogo.rb
+++ app/models/admin/catalogos/catalogo.rb
@@ -1,9 +1,12 @@
 class Admin::Catalogo < Catalogo
 
-  has_many :especies_catalogos, class_name: Admin::EspecieCatalogo, foreign_key: attribute_alias(:id)
-  has_many :especies, through: :especies_catalogos
+  has_many :especies_catalogo, class_name: Admin::EspecieCatalogo, foreign_key: attribute_alias(:id)
+  has_many :especies, through: :especies_catalogo, source: :especie
 
   scope :usos_count, -> { select(:id, :descripcion).select('COUNT(*) AS totales').group(:id, :descripcion) }
-  scope :usos, -> { usos_count.joins(:especies_catalogos).where(nivel1: 11).order(:descripcion) }
+  scope :usos, -> { usos_count.joins(:especies_catalogo).where(nivel1: 11).order(:descripcion) }
+
+  accepts_nested_attributes_for :especies_catalogo, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :especies, reject_if: :all_blank, allow_destroy: true
 
 end
--- app/models/admin/catalogos/especie_catalogo.rb
+++ app/models/admin/catalogos/especie_catalogo.rb
@@ -1,5 +1,6 @@
 class Admin::EspecieCatalogo < EspecieCatalogo
 
   has_many :especies, class_name: Admin::Especie, foreign_key: attribute_alias(:especie_id)
+  belongs_to :especie, foreign_key: attribute_alias(:especie_id)
 
 end
