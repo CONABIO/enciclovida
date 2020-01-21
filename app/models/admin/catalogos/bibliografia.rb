d488fb142676f671f1f0cf57d0ba38d1522aee98
Carlos Alonso <calonsogeek@gmail.com>
2020-01-21 13:53:34 -0600

    scope para autocompletar bibliografia

--- app/models/admin/catalogos/bibliografia.rb
+++ app/models/admin/catalogos/bibliografia.rb
@@ -1,2 +1,5 @@
 class Admin::Bibliografia < Bibliografia
+
+  scope :autocompleta, ->(termino) { select(:id, :cita_completa).where("#{Bibliografia.attribute_alias(:cita_completa)} REGEXP ?", termino).limit(10) }
+
 end
