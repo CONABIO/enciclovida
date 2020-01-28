c7abcac77c72d2aa506bc411669e75284ec24b32
Carlos Alonso <calonsogeek@gmail.com>
2020-01-28 17:18:40 -0600

    jquery ui en el campo de region para autocompletar

--- app/assets/javascripts/admin/catalogos/catalogos_extra.js
+++ app/assets/javascripts/admin/catalogos/catalogos_extra.js
@@ -26,3 +26,25 @@ $('form').on('focus', '.biblio-autocomplete', function () {
         }
     });
 });
+
+$('form').on('focus', '.reg-autocomplete', function () {
+    var instancia = $(this).autocomplete( "instance" );
+    if (instancia !== undefined) return;
+
+    var reg_id = '#' + $(this).attr('id');
+    var region_id = reg_id.replace(/_reg$/, '_region_id');
+
+    $(this).autocomplete({
+        minLength: 2,
+        source: function( request, response ) {
+            $.getJSON( "/admin/regiones/autocompleta", request, function( data, status, xhr ) {
+                response( data );
+            });
+        },
+        select: function( event, ui ) {
+            $(region_id).val(ui.item.value);
+            $(reg_id).val(ui.item.label);
+            return false;
+        }
+    });
+});
--- app/models/admin/catalogos/especie_catalogo_region.rb
+++ app/models/admin/catalogos/especie_catalogo_region.rb
@@ -1,5 +1,7 @@
 class Admin::EspecieCatalogoRegion < EspecieCatalogoRegion
 
+  attr_accessor :reg
+
   has_many :bibliografias, class_name: 'Admin::EspecieCatalogoRegionBibliografia', primary_key: [attribute_alias(:especie_id),attribute_alias(:catalogo_id),attribute_alias(:region_id)], foreign_key: [attribute_alias(:especie_id),attribute_alias(:catalogo_id),attribute_alias(:region_id)]
 
   accepts_nested_attributes_for :bibliografias, reject_if: :all_blank, allow_destroy: true
