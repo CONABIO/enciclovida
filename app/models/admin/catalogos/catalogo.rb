543d767243a97fa57edd6a4a5b14cc233609b17b
Carlos Alonso <calonsogeek@gmail.com>
2020-01-29 18:01:44 -0600

    los niveles en los catalogos con select

--- app/models/admin/catalogos/catalogo.rb
+++ app/models/admin/catalogos/catalogo.rb
@@ -6,6 +6,30 @@ class Admin::Catalogo < Catalogo
   scope :usos_count, -> { select(:id, :descripcion).select('COUNT(*) AS totales').group(:id, :descripcion) }
   scope :usos, -> { usos_count.joins(:especies_catalogo).where(nivel1: 11).order(:descripcion) }
 
+  def dame_nivel1
+    Admin::Catalogo.where(nivel2: 0, nivel3: 0, nivel4: 0, nivel5: 0).where("#{Catalogo.attribute_alias(:nivel1)} > ?", 1).order(:descripcion).map{ |c| [c.descripcion, c.nivel1] }
+  end
+
+  def dame_nivel2
+    return [] unless nivel2.present?
+    Admin::Catalogo.where(nivel1: nivel1, nivel3: 0, nivel4: 0, nivel5: 0).where("#{Catalogo.attribute_alias(:nivel2)} > ?", 0).order(:descripcion).map{ |c| [c.descripcion, c.nivel2] }
+  end
+
+  def dame_nivel3
+    return [] unless nivel3.present?
+    Admin::Catalogo.where(nivel1: nivel1, nivel2: nivel2, nivel4: 0, nivel5: 0).where("#{Catalogo.attribute_alias(:nivel3)} > ?", 0).order(:descripcion).map{ |c| [c.descripcion, c.nivel3] }
+  end
+
+  def dame_nivel4
+    return [] unless nivel4.present?
+    Admin::Catalogo.where(nivel1: nivel1, nivel2: nivel2, nivel3: nivel3, nivel5: 0).where("#{Catalogo.attribute_alias(:nivel4)} > ?", 0).order(:descripcion).map{ |c| [c.descripcion, c.nivel4] }
+  end
+
+  def dame_nivel5
+    return [] unless nivel5.present?
+    Admin::Catalogo.where(nivel1: nivel1, nivel2: nivel2, nivel3: nivel3, nivel4: nivel4, nivel4: 0).where("#{Catalogo.attribute_alias(:nivel5)} > ?", 0).order(:descripcion).map{ |c| [c.descripcion, c.nivel5] }
+  end
+
   accepts_nested_attributes_for :especies_catalogo, reject_if: :all_blank, allow_destroy: true
 
 end
