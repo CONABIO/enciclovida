28b1fafbc8c0e59e2689822205958bd5dd58c7d4
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2020-06-02 06:41:10 -0500

    parche feo pero funcional

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -17,4 +17,5 @@ admin/admin.js admin/catalogos/catalogos_extra.js
 lib/bootstrap-select.min.css lib/bootstrap-select.min.js
 boots3-downgrade/bootstrap.js boots3-downgrade/bootstrap-sprockets.js
 boots3-downgrade/boots3/bootstrap/_bootstrap.scss boots3-downgrade/busquedas/avanzada/checklist.css boots3-downgrade/busquedas/region/por_region.css boots3-downgrademetamares/metamares.css boots3-downgrade/metamares/graficas.css
+jquery-ui.js
 )
