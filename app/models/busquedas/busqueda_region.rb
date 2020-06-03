5edb1547abbc0583b1ad57360569969ef118d4d7
Carlos Alonso <calonsogeek@gmail.com>
2020-06-03 18:13:50 -0500

    quitando ese asqueroso parche de las correspondencias con el SNIB

--- app/models/busquedas/busqueda_region.rb
+++ app/models/busquedas/busqueda_region.rb
@@ -1,11 +1,6 @@
 class BusquedaRegion < Busqueda
   attr_accessor :resp, :key_especies, :key_especies_con_filtro, :url_especies
 
-  # Esta correspondencia no deberia existir pero las regiones en el snib las hicieron con las patas
-  CORRESPONDENCIA = [nil, '08', '01', '07', '23', '26', '10', '32', '16', '13', '24', '25', '04',
-                     '06', '31', '12', '20', '18', '14', '02', '19', '21', '15', '27', '03', '11',
-                     '22', '30', '05', '28', '09', '29', '17']
-
   # REVISADO: Cache para obtener el conteo de especies por grupo
   def cache_conteo_por_grupo
     if params[:tipo_region].present?
@@ -180,15 +175,4 @@ class BusquedaRegion < Busqueda
     grupos
   end
 
-  # La correspondencia del estado en el servicio de Abraham, debio haber sido la llave priamria ...
-  def correspondencia_estado
-    valor = CORRESPONDENCIA[params[:estado_id].to_i]
-
-    if valor
-      self.params[:estado_id] = valor
-      self.resp = { estatus: true }
-    else
-      self.resp = { estatus: false, msg: "El parámetro 'estado_id' no es es correcto." }
-    end
-  end
 end
