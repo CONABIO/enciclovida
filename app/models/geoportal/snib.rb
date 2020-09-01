36dd34fbfa1b5b6a49d1927bd0e59797c78dbca4
Carlos Alonso <calonsogeek@gmail.com>
2020-09-01 02:49:31 -0500

    consultando las especies con sus registros desde el modelo del snib en rails

--- app/models/geoportal/snib.rb
+++ app/models/geoportal/snib.rb
@@ -1,5 +1,25 @@
 class Geoportal::Snib < GeoportalAbs
 
   self.table_name = 'snib'
+  self.primary_key = 'idejemplar'
+
+  attr_accessor :region_id, :tipo_region, :resp
+
+  def dame_especies_por_region
+    return {} unless (tipo_region.present? && region_id.present?)
+
+    self.resp = Rails.cache.fetch("br_#{tipo_region}_#{region_id}", expires_in: eval(CONFIG.cache.busquedas_region)) do
+      consulta_especies_por_region
+    end
+  end
+  
+
+  private
+
+  def consulta_especies_por_region
+    resp = Geoportal::Snib.select('idnombrecatvalido, COUNT(*) AS nregistros').where("#{tipo_region}='#{region_id}' AND idnombrecatvalido <> '' AND especievalidabusqueda <> '' AND comentarioscatvalido LIKE 'Validado completamente con CAT.%'").group(:idnombrecatvalido).order('nregistros DESC')
+
+    resp.map{ |r| {r.idnombrecatvalido => r.nregistros} }.reduce({}, :merge)
+  end  
 
 end
