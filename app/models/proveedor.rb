65503fbd9be97c396f5aafbad9bc3734a4559b92
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2020-09-29 20:24:32 -0500

    ahora si, mejor poner una url q unca bajaran

--- app/models/proveedor.rb
+++ app/models/proveedor.rb
@@ -544,17 +544,19 @@ class Proveedor < ActiveRecord::Base
 
       # Para las observaciones de grado cientifico, verde
       self.kml << "<Style id=\"Placemark_cientifico\">\n"
-      self.kml << "<IconStyle>\n"
-      self.kml << "<color>00ff00</color>\n"
-      self.kml << "<scale>3</scale>\n"
+      self.kml << "<IconStyle><scale>1.5</scale>\n"
+      self.kml << "<Icon>\n"
+      self.kml << "<href>https://maps.google.com/mapfiles/kml/paddle/grn-blank.png</href>\n"
+      self.kml << "</Icon>\n"
       self.kml << "</IconStyle>\n"
       self.kml << "</Style>\n"
 
       # Para las observaciones de grado casual, amarillo
       self.kml << "<Style id=\"Placemark_casual\">\n"
-      self.kml << "<IconStyle>\n"
-      self.kml << "<color>ffff99</color>\n"
-      self.kml << "<scale>3</scale>\n"
+      self.kml << "<IconStyle><scale>1.5</scale>\n"
+      self.kml << "<Icon>\n"
+      self.kml << "<href>https://maps.google.com/mapfiles/kml/paddle/ylw-blank.png</href>\n"
+      self.kml << "</Icon>\n"
       self.kml << "</IconStyle>\n"
       self.kml << "</Style>\n"
     end
@@ -682,9 +684,10 @@ class Proveedor < ActiveRecord::Base
     self.kml << "<kml xmlns=\"http://www.opengis.net/kml/2.2\">\n"
     self.kml << "<Document>\n"
     self.kml << "<Style id=\"normalPlacemark\">\n"
-    self.kml << "<IconStyle>\n"
-    self.kml << "<color>ff0000</color>\n"
-    self.kml << "<scale>3</scale>\n"
+    self.kml << "<IconStyle><scale>1.5</scale>\n"
+    self.kml << "<Icon>\n"
+    self.kml << "<href>https://maps.google.com/mapfiles/kml/paddle/red-blank.png</href>\n"
+    self.kml << "</Icon>\n"
     self.kml << "</IconStyle>\n"
     self.kml << "</Style>\n"
 
