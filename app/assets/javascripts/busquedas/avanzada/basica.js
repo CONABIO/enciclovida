6c92069d0749669e03a344db0d527d69086806c5
Carlos Alonso <calonsogeek@gmail.com>
2020-11-04 02:59:16 -0600

    arreglando la descarga en la busqueda basica, ni siquiera funcionaba

--- app/assets/javascripts/busquedas/avanzada/basica.js
+++ app/assets/javascripts/busquedas/avanzada/basica.js
@@ -42,4 +42,56 @@ $(document).ready(function(){
         if ($("#resultados-" + settings.cat).html().length == 0)
             $("#resultados-" + settings.cat).load(url);
     });
+
+    // Para validar una ultima vez cuando paso la validacion del boton
+    $('#modal-descarga-avanzada').on('click', '.boton-descarga', function(){
+        var url_xlsx = datos_descarga.url.replace("resultados?", "resultados.xlsx?");
+        var correo = $('#modal-descarga-avanzada input[name=correo]').val();
+        var form_serialize = $('#modal-descarga-avanzada form').serialize();
+
+        if(correoValido(correo))
+        {
+            $.ajax({
+                url: url_xlsx + "&" + form_serialize,
+                type: 'GET',
+                dataType: "json"
+            }).done(function(resp) {
+                $('#modal-descarga-avanzada').modal('toggle');
+
+                if (resp.estatus == 1)
+                    $('#notice-avanzada').empty().html('!La petición se envió correctamente!. Se te enviará un correo con los resultados de tu búsqueda!').removeClass('d-none').slideDown(600);
+                else
+                    $('#notice-avanzada').empty().html('Lo sentimos no se pudo procesar tu petición, asegurate de haber anotado correctamente tu correo e inténtalo de nuevo.').removeClass('d-none').slideDown(600);
+
+            }).fail(function(){
+                $('#modal-descarga-avanzada').modal('toggle');
+                $('#notice-avanzada').empty().html('Lo sentimos no se pudo procesar tu petición, asegurate de haber anotado correctamente tu correo e inténtalo de nuevo.').removeClass('d-none').slideDown(600);
+            });
+
+        } else
+            return false;
+    });
+
+    // Para la validacion del correo en la descarga de la lista
+    dameValidacionCorreo('avanzada', '#notice-avanzada');
+
+    // Para validar una ultima vez cuando paso la validacion del boton y quiere descragar el checklist
+    $('#modal-descarga-checklist').on('click', '.boton-descarga', function(){
+        var url = $('#modal-menu-checklist').attr('url');
+
+        var campos = [];
+        $.each($('#modal-descarga-checklist form').serializeArray(), function (index, json) {
+            if (json.name == 'f_desc[]')
+                campos.push(json.name + '=' + json.value);
+        });
+
+        if (campos.length > 0) url = url + '&' + campos.join('&');
+
+        $('#modal-descarga-checklist').modal('toggle');
+        //$('#notice-avanzada').empty().html('!La petición se envió correctamente!. Se te enviará un correo con los resultados de tu búsqueda!').removeClass('d-none').slideDown(600);
+        window.open(url,'_blank');
+    });
+
+    // Para la validacion del correo en la descarga del checklist
+    dameValidacionCorreo('checklist', '#notice-avanzada');    
 });
