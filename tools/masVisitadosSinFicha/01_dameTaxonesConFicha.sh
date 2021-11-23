c10b2320711c3b5823fcf7b213a29d3a03d74c37
Carlos Alonso <calonsogeek@gmail.com>
2021-11-23 14:22:45 -0600

    ya no se ocupo ya que la ficha default no se guarda en cache

--- tools/masVisitadosSinFicha/01_dameTaxonesConFicha.sh
+++
@@ -1,2 +0,0 @@
-#Esta linea se debe correr desde /home/enciclovida/buscador/tmp/cache
-find /home/enciclovida/buscador/tmp/cache | grep especies%2F > ../../tools/masVisitadosSinFicha/taxonesConFicha.csv;
--- tools/masVisitadosSinFicha/02_dameTaxonesMasVisitados.rb
+++
@@ -1,3 +0,0 @@
-# Aqui va el código que saca las especies mas visitadas de la base,
-# asumo que es una consulta o con sql o con activRecord, puse terminación rb,
-# pero duede ser cualquiera
