0787816fb2fd846eeb63e9f577c65e0a324979be
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2022-09-26 09:46:15 -0500

    estilos de peces al show de especies

--- app/assets/javascripts/peces/peces_extra.js
+++ app/assets/javascripts/peces/peces_extra.js
@@ -1,47 +1,72 @@
 var limpiaBusqueda = function(){
-    var controles = ".porGrupo input, .porSemaforo input, .porSustentable input, .porZonas input, .porNombreGrupo input, .porNombreGrupo select, .porCriterios input, .porCriterios select";
-    var visuales = ".porGrupo span, .porSemaforo span, .porSustentable span, .porZonas span, .porZonas path, .porCriterios span";
-    var inputsALimpiar = $(controles + ", " + visuales);
-    inputsALimpiar.attr("disabled", false).removeClass("disabled zona-seleccionada").prop("checked", false);
-    $("#id, #nombre, .porNombreGrupo input, .porNombreGrupo select").val('');
-    $('#edo_cons').selectpicker('val','');
+	var controles = ".porGrupo input, .porSemaforo input, .porSustentable input, .porZonas input, .porNombreGrupo input, .porNombreGrupo select, .porCriterios input, .porCriterios select";
+	var visuales = ".porGrupo span, .porSemaforo span, .porSustentable span, .porZonas span, .porZonas path, .porCriterios span";
+	var inputsALimpiar = $(controles + ", " + visuales);
+	inputsALimpiar.attr("disabled", false).removeClass("disabled zona-seleccionada").prop("checked", false);
+	$("#id, #nombre, .porNombreGrupo input, .porNombreGrupo select").val('');
+	$('#edo_cons').selectpicker('val','');
 };
 
 var bloqueaBusqueda = function(){
-    var controles = ".porGrupo input, .porSemaforo input, .porSustentable input, .porZonas input, .porNombreGrupo select, .porCriterios input, .porCriterios select";
-    var visuales = ".porGrupo span, .porSemaforo span, .porSustentable span, .porZonas span, .porZonas path, .porCriterios span";
-    var inputsABloquear = $(controles + ", " + visuales);
-    inputsABloquear.attr("disabled", true).addClass("disabled").prop("checked", false);
+	var controles = ".porGrupo input, .porSemaforo input, .porSustentable input, .porZonas input, .porNombreGrupo select, .porCriterios input, .porCriterios select";
+	var visuales = ".porGrupo span, .porSemaforo span, .porSustentable span, .porZonas span, .porZonas path, .porCriterios span";
+	var inputsABloquear = $(controles + ", " + visuales);
+	inputsABloquear.attr("disabled", true).addClass("disabled").prop("checked", false);
 };
 
 var asignaFiltros = function(params)
 {
-    if (params.edo_cons != undefined) $('#edo_cons').selectpicker('val',params.edo_cons);
+	if (params.edo_cons != undefined) $('#edo_cons').selectpicker('val',params.edo_cons);
 };
 
 $(document).ready(function(){
-    TYPES = ['peces'];
-    soulmateAsigna('peces');
-
-
-
-    $("path[id^=path_zonas_]").on('click', function(){
-        $(this).toggleClass('zona-seleccionada');
-        var input = $('#' + this.id.replace('path_',''));
-        input.prop("checked", !input.prop("checked"));
-    });
-
-
-    $(".porZonas input").each(function(index, item){
-        if($(item).prop('checked')){
-            $('#path_' + item.id).addClass('zona-seleccionada');
-        }
-    });
-
-    $("html,body").animate({scrollTop: 105}, 500);
-
-    if($('#id').val()!=''){bloqueaBusqueda();}
-
+	TYPES = ['peces'];
+	soulmateAsigna('peces');
+	
+	$('[data-toggle="popover"]').one('click', function(){
+		var button = $(this);
+		var idEspecie = $(button).data('especie-id');
+		var pestaña = '/pmc/peces/'+idEspecie+'?mini=true';
+		$('[data-toggle="popover"]').popover('hide');
+		jQuery.get(pestaña).done(function(data){
+			button.popover({
+				html:true,
+				sanitize:false,
+				container: 'body',
+				placement: function(){
+					if($(window).width() < 990){
+						return 'bottom'
+					}else{
+						if(($(window).width() - button.offset().left) < $(window).width()/2){
+							return 'left';
+						}else{
+							return 'right';
+						}
+					}
+				},
+				trigger: 'manual',
+				content: data,
+			}).popover('show').attr('onclick',"$('[data-toggle=\"popover\"]').not(this).popover('hide'); $(this).popover('show');");
+		});
+	});
+	
+	$("path[id^=path_zonas_]").on('click', function(){
+		$(this).toggleClass('zona-seleccionada');
+		var input = $('#' + this.id.replace('path_',''));
+		input.prop("checked", !input.prop("checked"));
+	});
+	
+	
+	$(".porZonas input").each(function(index, item){
+		if($(item).prop('checked')){
+			$('#path_' + item.id).addClass('zona-seleccionada');
+		}
+	});
+	
+	$("html,body").animate({scrollTop: 105}, 500);
+	
+	if($('#id').val()!=''){bloqueaBusqueda();}
+	
 });
 
 var scroll_array = false;
