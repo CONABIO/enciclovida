4e9b800a1a6ea63ad1b895c7857952d7dbba5d2b
Julio Axel Gonzalez Morales <axell@MacBook-Air-de-Julio.local>
2026-06-15 12:15:59 -0600

    arreglando redis filtro a estado 0

--- app/models/estadisticas/especie_estadistica.rb
+++ app/models/estadisticas/especie_estadistica.rb
@@ -1,6 +1,7 @@
 class EspecieEstadistica < ActiveRecord::Base
 
   self.table_name = "#{CONFIG.bases.ev}.especies_estadistica"
+  self.primary_key = :id
 
   belongs_to :estadistica, inverse_of: :especie_estadisticas
   belongs_to :especie, :foreign_key => Especie.attribute_alias(:id), inverse_of: :especie_estadisticas
