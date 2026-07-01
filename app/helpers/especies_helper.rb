8d5787240ea5f0832f82dbb15dc46ed532ffce0c
Julio Axel Gonzalez Morales <axell@MacBook-Air-de-Julio.local>
2026-07-01 09:54:39 -0600

    cambiar texto de observaciones

--- app/helpers/especies_helper.rb
+++ app/helpers/especies_helper.rb
@@ -93,6 +93,7 @@ module EspeciesHelper
       descripciones_unicas = items.flat_map { |item| item[:descripciones] }.uniq
       # Usar las observaciones del primer item 
       observaciones = items.first[:observaciones]
+      observaciones = observaciones.gsub("ver Otras observaciones", "ver notas")
 
       # Crear un hash unificado por cada catálogo
       { nombre_catalogo: catalogo, descripciones: descripciones_unicas, observaciones: observaciones }
