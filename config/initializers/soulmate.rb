87cd0e120f3e8029003d1ee89499acf77e861ada
Julio Axel Gonzalez Morales <axell@MacBook-Air-de-Julio.local>
2026-07-08 11:41:27 -0600

    cambio de ip de redis

--- config/initializers/soulmate.rb
+++ config/initializers/soulmate.rb
@@ -1,4 +1,4 @@
-Soulmate.redis = 'redis://127.0.0.1:6379/0'
+Soulmate.redis = 'redis://172.18.0.1:6379/0'
 # or you can asign an existing instance of Redis, Redis::Namespace, etc.
 # Soulmate.redis = $redis
 Soulmate.stop_words = []
