2a94672c80c322f2fafe2c8c41f1ebcd4cec3303
Julio Axel Gonzalez Morales <axell@MacBook-Air-de-Julio.local>
2026-07-08 17:29:53 -0600

    nuevo export blurr sin troll

--- tools/enciclovida/exporta_blurrily.rb
+++ tools/enciclovida/exporta_blurrily.rb
@@ -1,24 +1,11 @@
 require 'rubygems'
-require 'trollop'
-
-OPTS = Trollop::options do
-  banner <<-EOS
-Exporta todas las tuplas de la base al servidor blurrilly:
-
-Usage:
-
-  rails r tools/exporta_blurrily.rb -d
-
-where [options] are:
-  EOS
-  opt :debug, 'Print debug statements', :type => :boolean, :short => '-d'
-end
 
 def exporta_a_blurrilly
-  Rails.logger.debug 'Exportando nombres cientificos ... ' if OPTS[:debug]
+  puts 'Exportando nombres científicos...'
+
   client_cientifico = Blurrily::Client.new(
-  host: ENV.fetch('BLURRILY_HOST', '127.0.0.1'),
-  db_name: 'nombres_cientificos'
+    host: ENV.fetch('BLURRILY_HOST', '127.0.0.1'),
+    db_name: 'nombres_cientificos'
   )
 
   client_comun = Blurrily::Client.new(
@@ -26,38 +13,48 @@ def exporta_a_blurrilly
     db_name: 'nombres_comunes'
   )
 
+  contador = 0
+
   Especie.find_each do |taxon|
-    Rails.logger.debug "#{taxon.id}-#{taxon.nombre_cientifico}"
     client_cientifico.put(taxon.nombre_cientifico, taxon.id)
+
+    contador += 1
+    puts "#{contador} especies indexadas..." if (contador % 1000).zero?
   end
 
-  Rails.logger.debug 'Exportando nombres comunes ... ' if OPTS[:debug]
+  puts 'Exportando nombres comunes...'
+
+  contador = 0
+
   NombreComun.find_each do |nom|
-    Rails.logger.debug "#{nom.id}-#{nom.nombre_comun}"
     client_comun.put(nom.nombre_comun, nom.id)
+
+    contador += 1
+    puts "#{contador} nombres comunes indexados..." if (contador % 1000).zero?
   end
 end
 
 def creando_carpeta
-  Rails.logger.debug "Creando carpeta \"#{@path}\" si es que no existe..." if OPTS[:debug]
-  Dir.mkdir(@path, 0755) unless File.exists?(@path)
+  puts "Creando carpeta \"#{@path}\" si no existe..."
+  Dir.mkdir(@path, 0755) unless File.exist?(@path)
 end
 
 def delete_files
-  Rails.logger.debug 'Eliminando archivos anteriores...' if OPTS[:debug]
-  f_cien="#{@path}/nombres_cientificos.trigrams"
-  f_com="#{@path}/nombres_comunes.trigrams"
+  puts 'Eliminando índices anteriores...'
 
-  File.delete(f_cien) if File.exists?(f_cien)
-  File.delete(f_com) if File.exists?(f_com)
+  f_cien = "#{@path}/nombres_cientificos.trigrams"
+  f_com = "#{@path}/nombres_comunes.trigrams"
+
+  File.delete(f_cien) if File.exist?(f_cien)
+  File.delete(f_com) if File.exist?(f_com)
 end
-
 
 start_time = Time.now
 
 @path = 'db/blurrily'
+
 creando_carpeta
 delete_files
 exporta_a_blurrilly
 
-Rails.logger.debug "Termino en #{Time.now - start_time} seg" if OPTS[:debug]
+puts "Terminado en #{(Time.now - start_time).round(2)} segundos."
