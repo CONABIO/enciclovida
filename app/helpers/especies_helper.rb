5bb8d2230ecb91560ff516ba24f71b73b9a51594
Julio Axel Gonzalez Morales <axell@MacBook-Air-de-Julio.local>
2026-07-24 16:29:57 -0600

    fix xeno en vistas

--- app/helpers/especies_helper.rb
+++ app/helpers/especies_helper.rb
@@ -348,28 +348,28 @@ module EspeciesHelper
 
   def imprime_canto(item)
     copyright = "Xeno Canto: Sharing bird sounds from around the world"
-    link_to image_tag("https:#{item['sono']['med']}"), "",  class: "m-1 modal-buttons", data: {
-        toggle: "modal",
-        target: "#modal_reproduce",
-        type: "video",
-        url: "#{item['file']}",
-        observation: "https://#{item['url']}",
-        file: "#{item['file']}",
-        generic_name: item['gen'],
-        specific_name: item['sp'],
-        subspecies_name: item['ssp'],
-        english_name: item['en'],
-        country_recording: item['cnt'],
-        locality: item['loc'],
-        # url: item['url'], #
-        file_name: item['file-name'],
-        license: item['lic'],
-        length: item['length'],
-        time: item['time'],
-        date: item['date'],
-        remarks: item['rmk'],
-        copyright: copyright,
-        author: item["rec"]
+
+    link_to image_tag(item['sono']['med']), "", class: "m-1 modal-buttons", data: {
+      toggle: "modal",
+      target: "#modal_reproduce",
+      type: "video",
+      url: item['file'],
+      observation: item['url'],
+      file: item['file'],
+      generic_name: item['gen'],
+      specific_name: item['sp'],
+      subspecies_name: item['ssp'],
+      english_name: item['en'],
+      country_recording: item['cnt'],
+      locality: item['loc'],
+      file_name: item['file-name'],
+      license: item['lic'],
+      length: item['length'],
+      time: item['time'],
+      date: item['date'],
+      remarks: item['rmk'],
+      copyright: copyright,
+      author: item['rec']
     }
   end
 
